c6f0ab91b32e8738543694b7c730495136eef552
Mikhail Zarechenskiy <zarechenskij@gmail.com>
2013-04-08 19:28:34 +0400

    Kotlin manager for storing psi-files

---
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/builder/KotlinManager.java
@@ -0,0 +1,89 @@
+package org.jetbrains.kotlin.core.builder;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
+import org.jetbrains.jet.plugin.JetFileType;
+import org.jetbrains.kotlin.parser.KotlinParser;
+
+import com.intellij.lang.ASTNode;
+
+public class KotlinManager {
+    
+    private final static Map<IProject, List<IFile>> projectFiles = new HashMap<>();
+    private final static Map<IFile, ASTNode> psiFiles = new HashMap<>();
+    
+    public final static String binFolder = "bin";
+
+    public static void updateProjectPsiSources(IProject project, IFile file, int flag) {
+        switch (flag) {
+            case IResourceDelta.ADDED:
+                assert !containsPsiFile(file) : "File(" + file.getName() + ") is already added"; 
+                
+                List<IFile> iFiles = projectFiles.get(project);
+                if (iFiles == null) {
+                    projectFiles.put(project, new ArrayList<IFile>());
+                }
+                projectFiles.get(project).add(file);
+                psiFiles.put(file, KotlinParser.parse(file));
+                break;
+                
+            case IResourceDelta.CHANGED:
+                assert containsPsiFile(file) : "File(" + file.getName() + ") does not contain in the psiFiles";
+                
+                psiFiles.put(file, KotlinParser.parse(file));
+                break;
+                
+            case IResourceDelta.REMOVED:
+                assert containsPsiFile(file) : "File(" + file.getName() + ") does not contain in the psiFiles";
+                
+                psiFiles.remove(file);
+                List<IFile> files = projectFiles.get(project);
+                files.remove(file);
+                break;
+                
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+    
+    public static void updateProjectPsiSources(IResource resource, int flag) {
+        updateProjectPsiSources(resource.getProject(), (IFile) resource, flag);
+    }
+    
+    public static Collection<ASTNode> getAllPsiFiles() {
+        return Collections.unmodifiableCollection(psiFiles.values());
+    }
+    
+    public static ArrayList<ASTNode> getPsiFiles(IProject project) {
+        List<IFile> filesByProject = projectFiles.get(project);
+        ArrayList<ASTNode> psiFilesByProject = new ArrayList<>();
+        if (filesByProject == null) {
+            return psiFilesByProject; 
+        }
+        
+        for (IFile file : filesByProject) {
+            psiFilesByProject.add(psiFiles.get(file));
+        }
+        
+        return psiFilesByProject;
+    }
+    
+    public static boolean isCompatibleResource(IResource resource) {
+        return resource.getType() == IResource.FILE && 
+                resource.getFileExtension().equals(JetFileType.INSTANCE.getDefaultExtension()) &&
+                !resource.getFullPath().segment(1).contentEquals(binFolder);
+    }
+    
+    private static boolean containsPsiFile(IFile file) {
+        return psiFiles.containsKey(file);
+    }
+}
