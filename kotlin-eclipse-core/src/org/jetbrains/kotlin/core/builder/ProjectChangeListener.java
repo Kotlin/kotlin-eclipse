72ee93e24fc60b0a877f7a02088b9110f61032bb
Mikhail Zarechenskiy <zarechenskij@gmail.com>
2013-04-17 20:06:42 +0400

    Code to debug analyzer

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/builder/ProjectChangeListener.java
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/builder/ProjectChangeListener.java
@@ -1,9 +1,16 @@
 package org.jetbrains.kotlin.core.builder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.runtime.CoreException;
+import org.jetbrains.jet.lang.diagnostics.Diagnostic;
+import org.jetbrains.jet.lang.diagnostics.Severity;
+import org.jetbrains.jet.lang.resolve.BindingContext;
+import org.jetbrains.kotlin.core.resolve.KotlinAnalyzer;
 
 public class ProjectChangeListener implements IResourceDeltaVisitor {
 
@@ -12,6 +19,13 @@ public class ProjectChangeListener implements IResourceDeltaVisitor {
         IResource resource = delta.getResource();
         if (KotlinManager.isCompatibleResource(resource)) {
             KotlinManager.updateProjectPsiSources(resource, delta.getKind());
+            
+            BindingContext bindingContext = KotlinAnalyzer.Analyze();
+            List<Diagnostic> diagnostics = new ArrayList<Diagnostic>(bindingContext.getDiagnostics());
+            for (Diagnostic diagnostic : diagnostics) {
+                System.out.println("Error: " + (diagnostic.getSeverity() == Severity.ERROR) + 
+                        " \"" + diagnostic.getPsiElement().getText() + "\" in " + diagnostic.getPsiFile().getName());
+            }
         }
         
         return true;
