850d967efb64e06d9552f264067b6f5efe26dfb0
Mikhail Zarechenskiy <rostov93@gmail.com>
2013-04-29 23:21:38 +0400

    Removed temp code

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/KotlinAnalyzer.java
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/KotlinAnalyzer.java
@@ -1,21 +1,15 @@
 package org.jetbrains.kotlin.core.resolve;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.jetbrains.jet.analyzer.AnalyzeExhaust;
-import org.jetbrains.jet.lang.descriptors.ModuleDescriptor;
-import org.jetbrains.jet.lang.descriptors.annotations.AnnotationDescriptor;
-import org.jetbrains.jet.lang.descriptors.impl.NamespaceDescriptorImpl;
 import org.jetbrains.jet.lang.psi.JetFile;
 import org.jetbrains.jet.lang.resolve.BindingContext;
 import org.jetbrains.jet.lang.resolve.java.AnalyzerFacadeForJVM;
-import org.jetbrains.jet.lang.resolve.name.Name;
-import org.jetbrains.jet.lang.resolve.scopes.WritableScope;
 import org.jetbrains.jet.lang.types.lang.KotlinBuiltIns;
 import org.jetbrains.kotlin.core.builder.KotlinManager;
 import org.jetbrains.kotlin.core.utils.KotlinEnvironment;
@@ -30,7 +24,6 @@ import com.intellij.psi.PsiManager;
 public class KotlinAnalyzer {
     private final Project ideaProject;
     private final JavaCoreApplicationEnvironment applicationEnvironment;
-    private BindingContext bindingContext;
 
     public KotlinAnalyzer() {
         KotlinEnvironment kotlinEnvironment = new KotlinEnvironment();
@@ -63,41 +56,5 @@ public class KotlinAnalyzer {
         }
         
         return jetFiles;
-    }
-
-    /*
-     * private static void scanPsiElements(ASTNode node) {
-     * if (node instanceof PsiElement) {
-     * if (psiTree == null) {
-     * psiTree = node.getPsi();
-     * } else {
-     * psiTree.add(node.getPsi());
-     * }
-     * }
-     * ASTNode[] children = node.getChildren(null);
-     * for (ASTNode child : children) {
-     * scanPsiElements(child);
-     * }
-     * }
-     */
-
-    private static class FakeJetNamespaceDescriptor extends NamespaceDescriptorImpl {
-        private WritableScope memberScope;
-
-        private FakeJetNamespaceDescriptor() {
-            super(new NamespaceDescriptorImpl(new ModuleDescriptor(Name.special("<fake_module>")),
-                    Collections.<AnnotationDescriptor> emptyList(), Name.special("<root>")),
-                    Collections.<AnnotationDescriptor> emptyList(),
-                    KotlinBuiltIns.getInstance().getBuiltInsPackage().getName());
-        }
-
-        void setMemberScope(WritableScope memberScope) {
-            this.memberScope = memberScope;
-        }
-
-        @Override
-        public WritableScope getMemberScope() {
-            return memberScope;
-        }
     }
 }
