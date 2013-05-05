d0be6ee575aad98bd2f131c3f90d4f5c4bcdb334
Mikhail Zarechenskiy <rostov93@gmail.com>
2013-05-05 21:09:33 +0400

    Add listener when Editor opened

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/KotlinEditor.java
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/KotlinEditor.java
@@ -1,14 +1,20 @@
 package org.jetbrains.kotlin.ui.editors;
 
+import static org.eclipse.core.resources.ResourcesPlugin.getWorkspace;
+
+import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.jface.text.ITextViewerExtension;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.ide.ResourceUtil;
+import org.jetbrains.kotlin.ui.AnnotationUpdater;
 
 public class KotlinEditor extends TextEditor {
 
     private final ColorManager colorManager;
     private final BracketInserter bracketInserter;
+    private AnnotationUpdater annotationUpdater;
     
     public KotlinEditor() {
         super();
@@ -19,6 +25,7 @@ public class KotlinEditor extends TextEditor {
         setDocumentProvider(new DocumentProvider());
     }
 
+    @SuppressWarnings("restriction")
     @Override
     public void createPartControl(Composite parent) {
         super.createPartControl(parent);
@@ -31,11 +38,21 @@ public class KotlinEditor extends TextEditor {
             bracketInserter.addBrackets('\'', '\'');
             bracketInserter.addBrackets('[', ']');        
             ((ITextViewerExtension) sourceViewer).prependVerifyKeyListener(bracketInserter);
+            
+            AnnotationManager.addAnnotationsToFile(ResourceUtil.getFile(this.getEditorInput()), this);
+            
+            annotationUpdater = new AnnotationUpdater(ResourceUtil.getFile(this.getEditorInput()), this);
+            getWorkspace().addResourceChangeListener(annotationUpdater, IResourceChangeEvent.POST_CHANGE);
         }
     }
 
+    @SuppressWarnings("restriction")
     @Override
     public void dispose() {
+        if (annotationUpdater != null) {
+            getWorkspace().removeResourceChangeListener(annotationUpdater);
+        }
+        
         colorManager.dispose();
         ISourceViewer sourceViewer = getSourceViewer();
         if (sourceViewer instanceof ITextViewerExtension) {
