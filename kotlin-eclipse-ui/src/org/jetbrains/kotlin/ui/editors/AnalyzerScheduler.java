c6d567746b250123fa3365b884f06882ac175b0a
Mikhail Zarechenskiy <rostov93@gmail.com>
2013-05-16 01:05:57 +0400

    Add problem markers to show errors and warnings

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/AnalyzerScheduler.java
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/AnalyzerScheduler.java
@@ -9,7 +9,10 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
@@ -17,7 +20,9 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.jetbrains.jet.lang.diagnostics.Diagnostic;
+import org.jetbrains.jet.lang.diagnostics.rendering.DefaultErrorMessages;
 import org.jetbrains.jet.lang.resolve.BindingContext;
+import org.jetbrains.kotlin.core.builder.KotlinManager;
 import org.jetbrains.kotlin.core.resolve.KotlinAnalyzer;
 
 import com.intellij.openapi.util.TextRange;
@@ -70,28 +75,49 @@ public class AnalyzerScheduler extends Job {
                 
                 BindingContext bindingContext = KotlinAnalyzer.analyze();
                 
+                List<IFile> workspaceFiles = new ArrayList<IFile>(KotlinManager.getAllFiles());
+                for (IFile file : workspaceFiles) {
+                    try {
+                        file.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
+                    } catch (CoreException e) {
+                    }
+                }
+                
                 List<Diagnostic> diagnostics = new ArrayList<Diagnostic>(bindingContext.getDiagnostics());
                 Map<IFile, List<KotlinAnnotation>> annotations = new HashMap<IFile, List<KotlinAnnotation>>();
                 for (Diagnostic diagnostic : diagnostics) {
-                    IFile curFile = ResourcesPlugin.getWorkspace().getRoot().
-                            getFileForLocation(new Path(diagnostic.getPsiFile().getVirtualFile().getPath()));
-                    if (!fileSet.contains(curFile)) {
-                        continue;
-                    }
                     List<TextRange> ranges = diagnostic.getTextRanges();
                     if (ranges.isEmpty()) {
                         continue;
                     }
                     String annotationType = null;
+                    int problemSeverity = 0;
                     switch (diagnostic.getSeverity()) {
                         case ERROR:
                             annotationType = AnnotationManager.annotationErrorType;
+                            problemSeverity = IMarker.SEVERITY_ERROR;
                             break;
                         case WARNING:
                             annotationType = AnnotationManager.annotationWarningType;
+                            problemSeverity = IMarker.SEVERITY_WARNING;
                             break;
                         default:
                             continue;
+                    }
+                    
+                    IFile curFile = ResourcesPlugin.getWorkspace().getRoot().
+                            getFileForLocation(new Path(diagnostic.getPsiFile().getVirtualFile().getPath()));
+                    try {
+                        IMarker problemMarker = curFile.createMarker(IMarker.PROBLEM);
+                        problemMarker.setAttribute(IMarker.MESSAGE, DefaultErrorMessages.RENDERER.render(diagnostic));
+                        problemMarker.setAttribute(IMarker.SEVERITY, problemSeverity);
+                        problemMarker.setAttribute(IMarker.CHAR_START, ranges.get(0).getStartOffset());
+                        problemMarker.setAttribute(IMarker.CHAR_END, ranges.get(0).getEndOffset());
+                    } catch (Exception e) {
+                    }
+                    
+                    if (!fileSet.contains(curFile)) {
+                        continue;
                     }
                     if (annotations.get(curFile) == null) {
                         annotations.put(curFile, new LinkedList<KotlinAnnotation>());
@@ -99,6 +125,8 @@ public class AnalyzerScheduler extends Job {
                     List<KotlinAnnotation> annotationsList = annotations.get(curFile);
                     annotationsList.add(new KotlinAnnotation(ranges.get(0).getStartOffset(), 
                             ranges.get(0).getLength(), annotationType));
+                    
+                    
                 }
                 
                 if (canceling) {
