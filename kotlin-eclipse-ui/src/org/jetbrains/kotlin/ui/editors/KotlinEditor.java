7d659173135f167efb303d853093e43b7f84748a
Mikhail Zarechenskiy <rostov93@gmail.com>
2013-05-20 23:46:46 +0400

    Add listener when editor opens

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/KotlinEditor.java
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/KotlinEditor.java
@@ -1,14 +1,19 @@
 package org.jetbrains.kotlin.ui.editors;
 
+import static org.eclipse.core.resources.ResourcesPlugin.getWorkspace;
+
+import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.jface.text.ITextViewerExtension;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.ide.ResourceUtil;
 
 public class KotlinEditor extends TextEditor {
 
     private final ColorManager colorManager;
     private final BracketInserter bracketInserter;
+    private AnnotationUpdater annotationUpdater;
     
     public KotlinEditor() {
         super();
@@ -31,11 +36,23 @@ public class KotlinEditor extends TextEditor {
             bracketInserter.addBrackets('\'', '\'');
             bracketInserter.addBrackets('[', ']');        
             ((ITextViewerExtension) sourceViewer).prependVerifyKeyListener(bracketInserter);
+            
+            annotationUpdater = AnnotationUpdater.INSTANCE;
+            
+            AnalyzerScheduler.INSTANCE.addFile(ResourceUtil.getFile(this.getEditorInput()), this);
+            AnalyzerScheduler.INSTANCE.schedule();
+            
+            getWorkspace().addResourceChangeListener(annotationUpdater, IResourceChangeEvent.POST_CHANGE);
         }
     }
 
     @Override
     public void dispose() {
+        if (annotationUpdater != null) {
+            getWorkspace().removeResourceChangeListener(annotationUpdater);
+        }
+        AnalyzerScheduler.INSTANCE.excludeFile(ResourceUtil.getFile(this.getEditorInput()));
+
         colorManager.dispose();
         ISourceViewer sourceViewer = getSourceViewer();
         if (sourceViewer instanceof ITextViewerExtension) {
