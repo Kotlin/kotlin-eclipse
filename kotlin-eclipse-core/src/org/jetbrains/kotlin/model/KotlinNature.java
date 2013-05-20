32df9350ca4ca7bedaab4e76b65644fdbee6cae5
dmitriy.bandurin <dmitriybandurin@gmail.com>
2013-05-20 14:25:30 +0400

    New Kotlin Project Wizard
    Create simple java project
    Add Kotlin Nature
    Setup default JRE and source folder for project
    Open Java Perspective.

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/model/KotlinNature.java
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/model/KotlinNature.java
@@ -4,6 +4,8 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.internal.core.JavaProject;
 
 public class KotlinNature implements IProjectNature {
     private IProject project;
@@ -38,22 +40,43 @@ public class KotlinNature implements IProjectNature {
         }
         return false;
     }
-    
+
+    /**
+     * Add java nature and Kotlin nature. If project is null then ignore and do nothing
+     * @param project project in which we should add natures
+     * @throws CoreException if can't add nature.
+     */
     public static void addNature(final IProject project) throws CoreException {
         if (project == null) {
             return;
         }
-        
-        if (!KotlinNature.hasKotlinNature(project)) {           
-            final IProjectDescription description = project.getDescription();
-            final String[] ids = description.getNatureIds();
-            final String[] newIds = new String[ids == null ? 1 : ids.length + 1];
-            if (ids != null) {
-                System.arraycopy(ids, 0, newIds, 0, ids.length);                
-                newIds[newIds.length - 1] = KotlinNature.KOTLIN_NATURE;
-            }               
-            description.setNatureIds(newIds);
-            project.setDescription(description, null);
-        }        
+
+        if(!JavaProject.hasJavaNature(project)){
+            addJavaNature(project);
+        }
+
+        if (!KotlinNature.hasKotlinNature(project)) {
+            addKotlinNature(project);
+        }
+    }
+
+    private static void addJavaNature(IProject project) throws CoreException {
+        addNature(project, JavaCore.NATURE_ID);
+    }
+
+    private static void addKotlinNature(IProject project) throws CoreException {
+        addNature(project, KotlinNature.KOTLIN_NATURE);
+    }
+
+    private static void addNature(IProject project, String nature) throws CoreException {
+        final IProjectDescription description = project.getDescription();
+        final String[] prevNatures = description.getNatureIds();
+        final String[] newNatures = new String[prevNatures == null ? 1 : prevNatures.length + 1];
+        if (prevNatures != null) {
+            System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
+            newNatures[newNatures.length - 1] = nature;
+        }
+        description.setNatureIds(newNatures);
+        project.setDescription(description, null);
     }
 }
