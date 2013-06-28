4b1e890aea3e13be287835c9d03413cb189519ba
zarechenskiy <rostov93@gmail.com>
2013-06-28 16:45:29 +0400

    Implement launch configuration tabs

---
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/launch/KotlinMainLauncherTab.java
@@ -0,0 +1,56 @@
+package org.jetbrains.kotlin.ui.launch;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaMainTab;
+import org.eclipse.jdt.internal.ui.viewsupport.JavaUILabelProvider;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.ui.dialogs.ListDialog;
+import org.jetbrains.kotlin.core.builder.KotlinManager;
+import org.jetbrains.kotlin.core.utils.ProjectUtils;
+
+public class KotlinMainLauncherTab extends JavaMainTab implements ILaunchConfigurationTab {
+    
+    @Override
+    public void handleSearchButtonSelected() {
+        ListDialog dialog = new ListDialog(getShell());
+        dialog.setBlockOnOpen(true);
+        dialog.setMessage("Select a Kotlin file to run");
+        dialog.setTitle("Choose Kotlin Compilation Unit");
+        dialog.setContentProvider(new ArrayContentProvider());
+        dialog.setLabelProvider(new JavaUILabelProvider());
+
+        Collection<IFile> projectFiles = null;
+        projectFiles = KotlinManager.getFilesByProject(fProjText.getText());
+        if (projectFiles == null) {
+            projectFiles = KotlinManager.getAllFiles();
+        } 
+        
+        List<IFile> mainFiles = new ArrayList<IFile>();
+        for (IFile file : projectFiles) {
+            if (ProjectUtils.hasMain(file)) {
+                mainFiles.add(file);
+            }
+        }
+        
+        dialog.setInput(mainFiles);
+        
+        if (dialog.open() == Window.CANCEL) {
+            return;
+        }
+        
+        Object[] results = dialog.getResult();
+        if (results == null || results.length == 0) {
+            return;
+        }
+        
+        if (results[0] instanceof IFile) {
+            fMainText.setText(ProjectUtils.createPackageClassName(((IFile) results[0])).toString());
+        }
+    }
+}
---
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/launch/LaunchConfigurationTabGroup.java
@@ -0,0 +1,26 @@
+package org.jetbrains.kotlin.ui.launch;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaJRETab;
+
+public class LaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+    @Override
+    public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+        ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
+                new KotlinMainLauncherTab(),
+                new JavaArgumentsTab(),
+                new JavaJRETab(),
+                new JavaClasspathTab(),
+                new CommonTab()
+        };
+        setTabs(tabs);
+        
+    }
+
+}
