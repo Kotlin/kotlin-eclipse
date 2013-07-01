b8f0029f525e36d43e3d0a27dc379d00f7d519e2
zarechenskiy <rostov93@gmail.com>
2013-07-01 15:34:18 +0400

    Make core activator extendable of Plugin class

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/Activator.java
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/Activator.java
@@ -4,32 +4,37 @@ import static org.eclipse.core.resources.ResourcesPlugin.getWorkspace;
 
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.runtime.Plugin;
 import org.jetbrains.kotlin.core.builder.ResourceChangeListener;
-import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-public class Activator implements BundleActivator {
-	private static BundleContext context;
+public class Activator extends Plugin {
+    
+	private static Activator plugin;
 	
 	private final IResourceChangeListener resourceChangeListener = new ResourceChangeListener();
 
 	public static final String PLUGIN_ID = "org.jetbrains.kotlin.core";
+
+	public Activator() {
+	    plugin = this;
+	}
 	
-	static BundleContext getContext() {
-		return context;
+	public static Activator getDefault() {
+		return plugin;
 	}
 
 	@Override
     public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-		
 		getWorkspace().addResourceChangeListener(resourceChangeListener, IResourceChangeEvent.POST_CHANGE);
+		
+		super.start(bundleContext);
 	}
 
 	@Override
     public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-		
 		getWorkspace().removeResourceChangeListener(resourceChangeListener);
+		
+		plugin = null;
 	}
 }
