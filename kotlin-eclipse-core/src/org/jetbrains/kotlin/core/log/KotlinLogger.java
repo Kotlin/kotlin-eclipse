1ef9a14fba018972c2aefcbd34a1cd9f6fedb716
zarechenskiy <rostov93@gmail.com>
2013-07-01 15:47:03 +0400

    Add logger

---
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/log/KotlinLogger.java
@@ -0,0 +1,28 @@
+package org.jetbrains.kotlin.core.log;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.jetbrains.kotlin.core.Activator;
+
+public class KotlinLogger {
+
+    public static void log(IStatus status) {
+        Activator.getDefault().getLog().log(status);
+    }
+    
+    public static void log(int severity, String message, Throwable exception) {
+        log(new Status(severity, Activator.PLUGIN_ID, message, exception));
+    }
+    
+    public static void logError(Throwable exception) {
+        log(IStatus.ERROR, "Unexpected Exception", exception);
+    }
+    
+    public static void logError(String message, Throwable exception) {
+        log(IStatus.ERROR, message, exception);
+    }
+    
+    public static void logInfo(String message) {
+        log(IStatus.INFO, message, null);
+    }
+}
