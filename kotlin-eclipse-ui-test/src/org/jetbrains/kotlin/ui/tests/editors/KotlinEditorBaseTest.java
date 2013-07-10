ecba7321077c084978e7c809e3dcf0d5710e4bb8
zarechenskiy <rostov93@gmail.com>
2013-07-10 12:31:40 +0400

    Simple tests to test framework

--- kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/editors/KotlinEditorBaseTest.java
+++ kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/editors/KotlinEditorBaseTest.java
@@ -4,11 +4,37 @@ import junit.framework.Assert;
 
 import org.jetbrains.kotlin.ui.editors.KotlinEditor;
 import org.junit.Test;
+import org.jetbrains.kotlin.testframework.editor.*;
+import org.jetbrains.kotlin.testframework.utils.*;
 
 public class KotlinEditorBaseTest {
 	@Test
 	public void sampleTest() {
 		KotlinEditor kotlinEditor = new KotlinEditor();
 		Assert.assertNotNull(kotlinEditor);
+	}
+	
+	@Test
+	public void TextEditorTest() {
+		TextEditorTest textEditor = new TextEditorTest();
+		Assert.assertNotNull(textEditor);
+	}
+	
+	@Test
+	public void openEditorTest() {
+		TextEditorTest textEditor = new TextEditorTest();
+		Assert.assertNotNull(textEditor.createEditor("Test.kt", ""));
+	}
+	
+	@Test
+	public void getJavaProjectTest() {
+		TextEditorTest textEditor = new TextEditorTest();
+		Assert.assertNotNull(textEditor.getJavaProject());
+	}
+	
+	@Test
+	public void createTestProjectTest() {
+		TestJavaProject testProject = new TestJavaProject("test_project");
+		Assert.assertNotNull(testProject);
 	}
 }
