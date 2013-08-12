a7af22f503c23251ce8c8dc8e5f14d6da5d5f3a3
Mikhail Zarechenskiy <zarechenskij@gmail.com>
2013-08-12 19:13:40 +0400

    Remove unnecessary annotation updating after document saving

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/builder/KotlinBuilder.java
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/builder/KotlinBuilder.java
@@ -43,8 +43,6 @@ public class KotlinBuilder extends IncrementalProjectBuilder {
         List<Diagnostic> diagnostics = analyzer.getDiagnostics();
         Map<IFile, List<DiagnosticAnnotation>> annotations = DiagnosticAnnotationUtil.INSTANCE.handleDiagnostics(diagnostics);
         
-        DiagnosticAnnotationUtil.INSTANCE.updateActiveEditorAnnotations(annotations);
-        
         addMarkersToProject(annotations);
         
         return null;
