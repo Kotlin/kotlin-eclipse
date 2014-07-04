d964f700e8a34ecdf441035365332af87aba6542
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2014-07-04 21:06:22 +0400

    Began to work on project creation operation, NewProjectWizard refactored and now extends AbstractProjectWizard.

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/wizards/ProjectCreationOp.java
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/wizards/ProjectCreationOp.java
@@ -16,15 +16,33 @@
  *******************************************************************************/
 package org.jetbrains.kotlin.wizards;
 
+import static org.eclipse.ui.ide.undo.WorkspaceUndoUtil.getUIInfoAdapter;
+
 import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.undo.CreateProjectOperation;
+import org.jetbrains.kotlin.core.log.KotlinLogger;
 
 public class ProjectCreationOp implements IRunnableWithProgress {
     
+    private final IProjectDescription projectDescription;
+    private final String projectName;
+    private final Shell shell;
+    
     private IProject result;
+    
+    public ProjectCreationOp(IProjectDescription projectDescription, String projectName, Shell shell) {
+        this.projectDescription = projectDescription;
+        this.projectName = projectName;
+        this.shell = shell;
+    }
     
     public IProject getResult() {
         return result;
@@ -32,7 +50,13 @@ public class ProjectCreationOp implements IRunnableWithProgress {
 
     @Override
     public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-        // TODO: add project creation
+        CreateProjectOperation operation = new CreateProjectOperation(projectDescription, projectName);
+        try {
+            PlatformUI.getWorkbench().getOperationSupport().getOperationHistory().execute(operation, monitor,
+                    getUIInfoAdapter(shell));
+        } catch (ExecutionException e) {
+            KotlinLogger.logAndThrow(e);
+        }
     }
 
 }
