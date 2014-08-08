71cec424911e98316d31fcd924969bfae866a6cd
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2014-08-08 22:24:08 +0400

    Activating Kotlin UI plugin on workbench startup.

---
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/KotlinUiStartupClass.java
@@ -0,0 +1,12 @@
+package org.jetbrains.kotlin.ui;
+
+import org.eclipse.ui.IStartup;
+
+public class KotlinUiStartupClass implements IStartup {
+    
+    @Override
+    public void earlyStartup() {
+        // activating Kotlin UI plugin on workbench startup
+    }
+    
+}
