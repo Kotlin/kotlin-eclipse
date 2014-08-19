4295c3a8cc5e5b48eb5dd2ff52418407a183ab00
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2014-08-19 19:05:24 +0400

    Tabs replaced with spaces.
    Minor: refactoring
    
    Conflicts:
    	kotlin-eclipse-test-framework/src/org/jetbrains/kotlin/testframework/utils/TestJavaProject.java

--- kotlin-eclipse-test-framework/src/org/jetbrains/kotlin/testframework/editor/KotlinEditorAutoTestCase.java
+++ kotlin-eclipse-test-framework/src/org/jetbrains/kotlin/testframework/editor/KotlinEditorAutoTestCase.java
@@ -27,23 +27,23 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 
 public abstract class KotlinEditorAutoTestCase extends KotlinEditorTestCase {
-
-	protected abstract static class EditorSourceFileData extends SourceFileData {
-
-		public EditorSourceFileData(File file) {
-			super(file.getName(), getText(file));
-		}
-	}
-	
-	protected static class WithAfterSourceFileData extends EditorSourceFileData {
+    
+    protected abstract static class EditorSourceFileData extends SourceFileData {
         
-	    private static final Predicate<WithAfterSourceFileData> TARGET_PREDICATE = new Predicate<WithAfterSourceFileData>() {
+        public EditorSourceFileData(File file) {
+            super(file.getName(), getText(file));
+        }
+    }
+    
+    protected static class WithAfterSourceFileData extends EditorSourceFileData {
+        
+        private static final Predicate<WithAfterSourceFileData> TARGET_PREDICATE = new Predicate<WithAfterSourceFileData>() {
             @Override
             public boolean apply(WithAfterSourceFileData data) {
                 return data.contentAfter != null;
             }
-	    };
-       
+        };
+        
         private static final String NO_TARGET_FILE_FOUND_ERROR_MESSAGE = "No target file found";
         private static final String NO_TARGET_FILE_FOUND_FOR_AFTER_FILE_ERROR_MESSAGE_FORMAT = "No target file found for \'%s\' file";
         
@@ -77,44 +77,46 @@ public abstract class KotlinEditorAutoTestCase extends KotlinEditorTestCase {
             
             WithAfterSourceFileData target = result.get(targetAfterFile.getName().replace(AFTER_FILE_EXTENSION, ""));
             if (target == null) {
-                throw new RuntimeException(String.format(NO_TARGET_FILE_FOUND_FOR_AFTER_FILE_ERROR_MESSAGE_FORMAT, targetAfterFile.getAbsolutePath()));
+                throw new RuntimeException(String.format(NO_TARGET_FILE_FOUND_FOR_AFTER_FILE_ERROR_MESSAGE_FORMAT,
+                        targetAfterFile.getAbsolutePath()));
             }
             
-            target.contentAfter = getText(targetAfterFile);       
+            target.contentAfter = getText(targetAfterFile);
             
             return result.values();
         }
         
         public static WithAfterSourceFileData getTargetFile(Iterable<WithAfterSourceFileData> files) {
-            return Iterables.<WithAfterSourceFileData>find(files, TARGET_PREDICATE, null);
+            return Iterables.<WithAfterSourceFileData> find(files, TARGET_PREDICATE, null);
         }
     }
-	
-	protected final void doAutoTest() {
-		String testPath = getTestDataPath() + name.getMethodName();
-		File testFolder = new File(testPath);
-		File testFile = new File(testPath + KT_FILE_EXTENSION);
-		
-		if (testFolder.exists() && testFolder.isDirectory()) {
-			doMultiFileAutoTest(testFolder);
-		} else if (testFile.exists() && testFile.isFile()) {
-			doSingleFileAutoTest(testPath + KT_FILE_EXTENSION);
-		} else {
-			throw new RuntimeException(String.format("Neither file \'%s\' nor directory \'%s\' was found", testFile.getAbsolutePath(), testFolder.getAbsolutePath()));
-		}
-	}
-	
-	private static final String TEST_DATA_PATH = "testData/";
-	
-	protected static final String KT_FILE_EXTENSION = ".kt";
-	protected static final String AFTER_FILE_EXTENSION = ".after";
-	protected static final String BEFORE_FILE_EXTENSION = ".before";
-	
-	protected abstract void doSingleFileAutoTest(String testPath);
-	
-	protected abstract void doMultiFileAutoTest(File testFolder);
-
-	protected String getTestDataPath() {
-		return TEST_DATA_PATH;
-	}
+    
+    protected final void doAutoTest() {
+        String testPath = getTestDataPath() + name.getMethodName();
+        File testFolder = new File(testPath);
+        File testFile = new File(testPath + KT_FILE_EXTENSION);
+        
+        if (testFolder.exists() && testFolder.isDirectory()) {
+            doMultiFileAutoTest(testFolder);
+        } else if (testFile.exists() && testFile.isFile()) {
+            doSingleFileAutoTest(testPath + KT_FILE_EXTENSION);
+        } else {
+            throw new RuntimeException(String.format("Neither file \'%s\' nor directory \'%s\' was found",
+                    testFile.getAbsolutePath(), testFolder.getAbsolutePath()));
+        }
+    }
+    
+    private static final String TEST_DATA_PATH = "testData/";
+    
+    protected static final String KT_FILE_EXTENSION = ".kt";
+    protected static final String AFTER_FILE_EXTENSION = ".after";
+    protected static final String BEFORE_FILE_EXTENSION = ".before";
+    
+    protected abstract void doSingleFileAutoTest(String testPath);
+    
+    protected abstract void doMultiFileAutoTest(File testFolder);
+    
+    protected String getTestDataPath() {
+        return TEST_DATA_PATH;
+    }
 }
--- kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/editors/KotlinOpenDeclarationTestCase.java
+++ kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/editors/KotlinOpenDeclarationTestCase.java
@@ -22,25 +22,25 @@ import org.jetbrains.kotlin.testframework.editor.KotlinEditorTestCase;
 import org.jetbrains.kotlin.testframework.utils.EditorTestUtils;
 
 public abstract class KotlinOpenDeclarationTestCase extends KotlinEditorTestCase {
-	
-	public void doTest(String inputFileName, String input, String expected) {
-		doTest(inputFileName, input, null, expected);
-	}
-	
-	protected void doTest(String inputFileName, String input, String referenceFileName, String referenceFile) {
-		testEditor = configureEditor(inputFileName, input);
-		
-		String expected = referenceFile;
-		if (referenceFileName != null) {
-			createSourceFile(referenceFileName, referenceFile);
-		}
-		
-		refreshWorkspace();
-		joinBuildThread();
-		
-    	testEditor.accelerateOpenDeclaration();
-
-		JavaEditor activeEditor = (JavaEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-		EditorTestUtils.assertByEditor(activeEditor, expected);
-	}
+    
+    public void doTest(String inputFileName, String input, String expected) {
+        doTest(inputFileName, input, null, expected);
+    }
+    
+    protected void doTest(String inputFileName, String input, String referenceFileName, String referenceFile) {
+        testEditor = configureEditor(inputFileName, input);
+        
+        String expected = referenceFile;
+        if (referenceFileName != null) {
+            createSourceFile(referenceFileName, referenceFile);
+        }
+        
+        refreshWorkspace();
+        joinBuildThread();
+        
+        testEditor.accelerateOpenDeclaration();
+        
+        JavaEditor activeEditor = (JavaEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+        EditorTestUtils.assertByEditor(activeEditor, expected);
+    }
 }
