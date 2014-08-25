2b889eb668cd48f269f64c398d938ce632039eaf
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2014-08-25 19:11:45 +0400

    Minor: java code conventions

--- kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/core/tests/launch/KotlinLaunchTest.java
+++ kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/core/tests/launch/KotlinLaunchTest.java
@@ -19,26 +19,26 @@ package org.jetbrains.kotlin.core.tests.launch;
 import org.junit.Test;
 
 public class KotlinLaunchTest extends KotlinLaunchTestCase {
-	
-	private static final String sourceCode = "fun main(args : Array<String>) {}";
-
-	@Test
-	public void launchSimpleProject() {
-		doTest(sourceCode, "test_project", "org.jet.pckg", null);
-	}
-	
-	@Test
-	public void launchWhenProjectNameHaveSpace() {
-		doTest(sourceCode, "test project", "pckg", null);
-	}
-	
-	@Test
-	public void launchWithTwoSourceFolders() {
-		doTest(sourceCode, "testProject", "pckg", "src2");
-	}
-	
-	@Test
-	public void launchWhenSourceFolderHaveSpace() {
-		doTest(sourceCode, "testProject", "pckg", "src directory");
-	}
+    
+    private static final String SOURCE_CODE = "fun main(args : Array<String>) {}";
+    
+    @Test
+    public void launchSimpleProject() {
+        doTest(SOURCE_CODE, "test_project", "org.jet.pckg", null);
+    }
+    
+    @Test
+    public void launchWhenProjectNameHaveSpace() {
+        doTest(SOURCE_CODE, "test project", "pckg", null);
+    }
+    
+    @Test
+    public void launchWithTwoSourceFolders() {
+        doTest(SOURCE_CODE, "testProject", "pckg", "src2");
+    }
+    
+    @Test
+    public void launchWhenSourceFolderHaveSpace() {
+        doTest(SOURCE_CODE, "testProject", "pckg", "src directory");
+    }
 }
