66398025e8ebc7f72814a0e430649a6447ee0b73
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2014-10-05 20:36:09 +0400

    Return ability to launch kotlin from editor

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/launch/KotlinLaunchableTester.java
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/launch/KotlinLaunchableTester.java
@@ -2,6 +2,7 @@ package org.jetbrains.kotlin.ui.launch;
 
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.jetbrains.jet.lang.psi.JetFile;
@@ -13,16 +14,18 @@ import org.jetbrains.kotlin.core.resolve.KotlinAnalyzer;
 public class KotlinLaunchableTester extends PropertyTester {
     @Override
     public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-        if (receiver instanceof IFile) {
-            IFile eclipseFile = (IFile) receiver;
-            JetFile jetFile = KotlinPsiManager.getKotlinParsedFile(eclipseFile);
-            if (jetFile == null) {
-                return false;
+        if (receiver instanceof IAdaptable) {
+            IFile file = (IFile) ((IAdaptable) receiver).getAdapter(IFile.class);
+            if (file != null) {
+                JetFile jetFile = KotlinPsiManager.getKotlinParsedFile(file);
+                if (jetFile == null) {
+                    return false;
+                }
+                
+                IJavaProject javaProject = JavaCore.create(file.getProject()); 
+                BindingContext bindingContext = KotlinAnalyzer.analyzeDeclarations(javaProject).getBindingContext();
+                return new MainFunctionDetector(bindingContext).hasMain(jetFile.getDeclarations());
             }
-            
-            IJavaProject javaProject = JavaCore.create(eclipseFile.getProject()); 
-            BindingContext bindingContext = KotlinAnalyzer.analyzeDeclarations(javaProject).getBindingContext();
-            return new MainFunctionDetector(bindingContext).hasMain(jetFile.getDeclarations());
         }
         
         return false;
