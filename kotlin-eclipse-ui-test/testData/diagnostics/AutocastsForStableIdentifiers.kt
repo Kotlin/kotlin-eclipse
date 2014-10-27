3e3d34aa2a468fb0dcdd0bee1e312d86d832f14e
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2014-10-27 20:42:57 +0400

    Update to 0.9.187 - Test data update

--- kotlin-eclipse-ui-test/testData/diagnostics/AutocastsForStableIdentifiers.kt
+++ kotlin-eclipse-ui-test/testData/diagnostics/AutocastsForStableIdentifiers.kt
@@ -20,53 +20,53 @@ val x : Any? = 1
 fun Any?.vars(<!UNUSED_PARAMETER!>a<!>: Any?) : Int {
     var <!ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE!>b<!>: Int = 0
     if (ns.y is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>ns.y<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>ns.y<!>
     }
     if (ns.y is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>example.ns.y<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>example.ns.y<!>
     }
     if (example.ns.y is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>ns.y<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>ns.y<!>
     }
     if (example.ns.y is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>example.ns.y<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>example.ns.y<!>
     }
 //    if (package.bottles.ns.y is Int) {
 //        b = ns.y
 //    }
     if (Obj.y is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>Obj.y<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>Obj.y<!>
     }
     if (example.Obj.y is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>Obj.y<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>Obj.y<!>
     }
     if (AClass.y is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>AClass.y<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>AClass.y<!>
     }
     if (example.AClass.y is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>AClass.y<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>AClass.y<!>
     }
     if (x is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>x<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>x<!>
     }
     if (example.x is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>x<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>x<!>
     }
     if (example.x is Int) {
-        b = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>example.x<!>
+        b = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>example.x<!>
     }
     return 1
 }
 
 fun Any?.foo() : Int {
     if (this is Int)
-      return <!DEBUG_INFO_AUTOCAST!>this<!>
+      return <!DEBUG_INFO_SMARTCAST!>this<!>
     if (this@foo is Int)
-      return <!DEBUG_INFO_AUTOCAST!>this<!>
+      return <!DEBUG_INFO_SMARTCAST!>this<!>
     if (this@foo is Int)
-      return <!DEBUG_INFO_AUTOCAST!>this@foo<!>
+      return <!DEBUG_INFO_SMARTCAST!>this@foo<!>
     if (this is Int)
-      return <!DEBUG_INFO_AUTOCAST!>this@foo<!>
+      return <!DEBUG_INFO_SMARTCAST!>this@foo<!>
     return 1
 }
 
@@ -77,16 +77,16 @@ open class C {
     fun foo() {
        var <!ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE!>t<!> : T? = null
        if (this is T) {
-          t = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>this<!>
+          t = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>this<!>
        }
        if (this is T) {
-          t = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>this@C<!>
+          t = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>this@C<!>
        }
        if (this@C is T) {
-          t = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>this<!>
+          t = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>this<!>
        }
        if (this@C is T) {
-          t = <!UNUSED_VALUE, DEBUG_INFO_AUTOCAST!>this@C<!>
+          t = <!UNUSED_VALUE, DEBUG_INFO_SMARTCAST!>this@C<!>
        }
     }
 }
