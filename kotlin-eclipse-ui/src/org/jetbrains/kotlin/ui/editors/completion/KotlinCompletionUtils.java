0005dc8ea4dbae38a64d5e936443f0b3cef406a8
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-01-21 16:59:20 +0300

    Minor: extract constant

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/completion/KotlinCompletionUtils.java
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/completion/KotlinCompletionUtils.java
@@ -7,12 +7,12 @@ import org.eclipse.jdt.core.search.SearchPattern;
 import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.kotlin.core.builder.KotlinPsiManager;
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor;
 import org.jetbrains.kotlin.eclipse.ui.utils.EditorUtil;
 import org.jetbrains.kotlin.eclipse.ui.utils.LineEndUtil;
 import org.jetbrains.kotlin.psi.JetFile;
 import org.jetbrains.kotlin.psi.JetSimpleNameExpression;
-import org.jetbrains.kotlin.core.builder.KotlinPsiManager;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
@@ -20,7 +20,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 
 public class KotlinCompletionUtils {
-
+    private static final String KOTLIN_DUMMY_IDENTIFIER = "KotlinRulezzz";
     public static final KotlinCompletionUtils INSTANCE = new KotlinCompletionUtils();
     
     private KotlinCompletionUtils() {
@@ -43,7 +43,7 @@ public class KotlinCompletionUtils {
     @Nullable
     public JetSimpleNameExpression getSimpleNameExpression(@NotNull JavaEditor editor, int identOffset) {
         String sourceCode = EditorUtil.getSourceCode(editor);
-        String sourceCodeWithMarker = new StringBuilder(sourceCode).insert(identOffset, "KotlinRulezzz").toString();
+        String sourceCodeWithMarker = new StringBuilder(sourceCode).insert(identOffset, KOTLIN_DUMMY_IDENTIFIER).toString();
         
         JetFile jetFile = KotlinPsiManager.INSTANCE.getParsedFile(EditorUtil.getFile(editor), sourceCodeWithMarker);
         
