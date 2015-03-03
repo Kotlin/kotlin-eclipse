1f78c4c1bfb729a4494d8869414f57e8820b0787
unknown <mikhail.zarechenskiy@jetbrains.com>
2015-03-03 14:23:21 +0300

    Support external projects (that are not under the workspace)

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/asJava/LightClassFile.java
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/asJava/LightClassFile.java
@@ -1,12 +1,13 @@
 package org.jetbrains.kotlin.core.asJava;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.kotlin.core.log.KotlinLogger;
+import org.jetbrains.kotlin.core.model.KotlinJavaManager;
 
 public class LightClassFile {
     private final IFile file;
@@ -21,7 +22,8 @@ public class LightClassFile {
     
     public void createNewFile() {
         try {
-            file.create(new ByteArrayInputStream(new byte[0]), true, null);
+            file.createLink(KotlinJavaManager.INSTANCE.setKotlinFileSystemScheme(file.getLocationURI()), 
+                    IResource.REPLACE | IResource.ALLOW_MISSING_LOCAL, null);
         } catch (CoreException e) {
             KotlinLogger.logAndThrow(e);
         }
@@ -29,7 +31,7 @@ public class LightClassFile {
     
     @NotNull
     public File asFile() {
-        return file.getRawLocation().makeAbsolute().toFile();
+        return new File(file.getLocationURI().getSchemeSpecificPart());
     }
     
     @NotNull
