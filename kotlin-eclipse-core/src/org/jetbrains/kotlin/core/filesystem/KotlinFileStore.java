e46cd66e991bed2e4f5c16ae323bbfd2afe960f5
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-06-15 13:40:27 +0300

    Make Kotlin light classes synchronized with workspace
    Now light classes are not removed after refreshing workspace

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/filesystem/KotlinFileStore.java
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/filesystem/KotlinFileStore.java
@@ -5,11 +5,16 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.filesystem.IFileInfo;
 import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.filesystem.provider.FileInfo;
 import org.eclipse.core.internal.filesystem.local.LocalFile;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
@@ -18,11 +23,13 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.internal.compiler.util.Util;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.analyzer.AnalysisResult;
 import org.jetbrains.kotlin.backend.common.output.OutputFile;
 import org.jetbrains.kotlin.codegen.state.GenerationState;
 import org.jetbrains.kotlin.core.asJava.KotlinLightClassGeneration;
+import org.jetbrains.kotlin.core.log.KotlinLogger;
 import org.jetbrains.kotlin.core.model.KotlinAnalysisProjectCache;
 import org.jetbrains.kotlin.psi.JetFile;
 
@@ -58,6 +65,42 @@ public class KotlinFileStore extends LocalFile {
     }
     
     @Override
+    public IFileInfo fetchInfo(int options, IProgressMonitor monitor) {
+        FileInfo info = (FileInfo) super.fetchInfo(options, monitor);
+        if (Util.isClassFileName(getName())) {
+            IFile workspaceFile = findFileInWorkspace();
+            if (workspaceFile != null) {
+                info.setExists(workspaceFile.exists());
+            }
+        } else {
+            IContainer workspaceContainer = findFolderInWorkspace();
+            if (workspaceContainer != null) {
+                info.setExists(workspaceContainer.exists());
+                info.setDirectory(true);
+            }
+        }
+        
+        return info;
+    }
+    
+    @Override
+    public String[] childNames(int options, IProgressMonitor monitor) {
+        List<String> children = new ArrayList<>();
+        try {
+            IContainer folder = findFolderInWorkspace();
+            if (folder != null && folder.exists()) {
+                for (IResource member : folder.members()) {
+                    children.add(member.getName());
+                }
+            }
+        } catch (CoreException e) {
+            KotlinLogger.logAndThrow(e);
+        }
+        
+        return children.toArray(new String[children.size()]);
+    }
+    
+    @Override
     public IFileStore mkdir(int options, IProgressMonitor monitor) throws CoreException {
         return this;
     }
@@ -89,12 +132,31 @@ public class KotlinFileStore extends LocalFile {
     }
     
     @Nullable
-    private IJavaProject getJavaProject() {
+    private IFile findFileInWorkspace() {
         IFile[] files = ResourcesPlugin.getWorkspace().getRoot().findFilesForLocationURI(file.toURI());
         if (files != null && files.length > 0) {
-            return JavaCore.create(files[0].getProject());
+            assert files.length == 1 : "By " + file.toURI() + "found more than one file";
+            
+            return files[0];
         }
         
         return null;
+    }
+    
+    @Nullable
+    private IContainer findFolderInWorkspace() {
+        IContainer[] containers = ResourcesPlugin.getWorkspace().getRoot().findContainersForLocationURI(file.toURI());
+        if (containers != null && containers.length > 0) {
+            assert containers.length == 1 : "By " + file.toURI() + "found more than one file";
+            return containers[0];
+        }
+        
+        return null;
+    }
+    
+    @Nullable
+    private IJavaProject getJavaProject() {
+        IFile file = findFileInWorkspace();
+        return file != null ? JavaCore.create(file.getProject()) : null;
     }
 }
