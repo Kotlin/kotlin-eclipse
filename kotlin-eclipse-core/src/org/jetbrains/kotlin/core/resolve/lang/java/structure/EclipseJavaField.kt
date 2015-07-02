300b2d79472fe94dab9f2119cea8f85888ca9896
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-07-02 20:33:53 +0300

    Rewrite to Kotlin

---
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/lang/java/structure/EclipseJavaField.kt
@@ -0,0 +1,30 @@
+/*******************************************************************************
+* Copyright 2000-2014 JetBrains s.r.o.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+package org.jetbrains.kotlin.core.resolve.lang.java.structure
+
+import org.eclipse.jdt.core.dom.IVariableBinding
+import org.jetbrains.kotlin.load.java.structure.JavaClass
+import org.jetbrains.kotlin.load.java.structure.JavaField
+import org.jetbrains.kotlin.load.java.structure.JavaType
+
+public class EclipseJavaField(private val javaField: IVariableBinding) : EclipseJavaMember<IVariableBinding>(javaField), JavaField {
+	override public fun isEnumEntry(): Boolean = getBinding().isEnumConstant()
+	
+	override public fun getType(): JavaType = EclipseJavaType.create(getBinding().getType())
+	
+	override public fun getContainingClass(): JavaClass = EclipseJavaClass(getBinding().getDeclaringClass())
+}
---
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/lang/kotlin/EclipseVirtualFileFinder.kt
@@ -0,0 +1,86 @@
+/*******************************************************************************
+* Copyright 2000-2014 JetBrains s.r.o.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+package org.jetbrains.kotlin.core.resolve.lang.kotlin
+
+import org.eclipse.core.resources.IResource
+import org.eclipse.core.runtime.IPath
+import org.eclipse.jdt.core.IJavaProject
+import org.eclipse.jdt.core.IType
+import org.eclipse.jdt.core.JavaModelException
+import org.jetbrains.kotlin.core.log.KotlinLogger
+import org.jetbrains.kotlin.core.model.KotlinEnvironment
+import org.jetbrains.kotlin.core.resolve.lang.java.EclipseJavaClassFinder
+import org.jetbrains.kotlin.load.java.structure.JavaClass
+import org.jetbrains.kotlin.load.kotlin.JvmVirtualFileFinder
+import org.jetbrains.kotlin.load.kotlin.JvmVirtualFileFinderFactory
+import org.jetbrains.kotlin.load.kotlin.KotlinBinaryClassCache
+import org.jetbrains.kotlin.load.kotlin.KotlinJvmBinaryClass
+import org.jetbrains.kotlin.load.kotlin.VirtualFileKotlinClassFinder
+import org.jetbrains.kotlin.name.ClassId
+import org.jetbrains.kotlin.name.FqName
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaClass
+import org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElement
+import org.eclipse.jdt.core.dom.ITypeBinding
+import org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementUtil
+import org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaClassifier
+
+public class EclipseVirtualFileFinder(val javaProject: IJavaProject) : VirtualFileKotlinClassFinder(), JvmVirtualFileFinderFactory {
+	override public fun findVirtualFileWithHeader(classId: ClassId): VirtualFile? {
+		val type = javaProject.findType(classId.getPackageFqName().asString(), classId.getRelativeClassName().asString())
+		if (type == null || !isBinaryKotlinClass(type)) return null
+		
+		// At first check that file exists in the workspace or not, if not then get absolute path
+		val path = type.getResource()?.getLocation() ?: type.getPath() 
+		val relativePath = "${type.getFullyQualifiedName().replace('.', '/')}.class"
+		
+		return KotlinEnvironment.getEnvironment(javaProject).getVirtualFileInJar(path, relativePath)
+	}
+	
+	private fun isBinaryKotlinClass(type: IType): Boolean = type.isBinary() && !EclipseJavaClassFinder.isInKotlinBinFolder(type)
+	
+	private fun classFileName(jClass:JavaClass): String {
+		val outerClass = jClass.getOuterClass()
+		if (outerClass == null) return jClass.getName().asString()
+		return classFileName(outerClass) + "$" + jClass.getName().asString()
+	}
+	
+	override public fun findKotlinClass(javaClass: JavaClass): KotlinJvmBinaryClass? {
+		val fqName = javaClass.getFqName()
+		if (fqName == null) return null
+		
+		val classId = EclipseJavaElementUtil.computeClassId((javaClass as EclipseJavaClassifier<*>).getBinding())
+		if (classId == null) return null
+		
+		var file = findVirtualFileWithHeader(classId)
+		if (file == null) return null
+		
+		if (javaClass.getOuterClass() != null) {
+			// For nested classes we get a file of the containing class, to get the actual class file for A.B.C,
+			// we take the file for A, take its parent directory, then in this directory we look for A$B$C.class
+			file = file.getParent().findChild("${classFileName(javaClass)}.class")
+			if (file != null) throw IllegalStateException("Virtual file not found for $javaClass")
+		}
+		
+		return KotlinBinaryClassCache.getKotlinBinaryClass(file!!)
+	}
+	
+	override public fun create(scope: GlobalSearchScope): JvmVirtualFileFinder {
+		return EclipseVirtualFileFinder(javaProject)
+	}
+}
