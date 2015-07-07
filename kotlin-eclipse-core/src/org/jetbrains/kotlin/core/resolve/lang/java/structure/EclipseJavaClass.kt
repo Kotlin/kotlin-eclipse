eb16086a93059f9288e6b2d884c95f2ad9f4ca07
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-07-07 19:54:11 +0300

    Rewrite to Kotlin: rewrite

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/lang/java/structure/EclipseJavaClass.kt
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/lang/java/structure/EclipseJavaClass.kt
@@ -1,198 +1,118 @@
 /*******************************************************************************
- * Copyright 2000-2014 JetBrains s.r.o.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *  
- * http://www.apache.org/licenses/LICENSE-2.0
- *   
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *******************************************************************************/
-package org.jetbrains.kotlin.core.resolve.lang.java.structure;
+* Copyright 2000-2014 JetBrains s.r.o.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+package org.jetbrains.kotlin.core.resolve.lang.java.structure
 
-import static org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementFactory.annotations;
-import static org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementFactory.classifierTypes;
-import static org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementFactory.fields;
-import static org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementFactory.methods;
-import static org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementFactory.typeParameters;
+import org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementFactory.annotations
+import org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementFactory.classifierTypes
+import org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementFactory.fields
+import org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementFactory.methods
+import org.jetbrains.kotlin.core.resolve.lang.java.structure.EclipseJavaElementFactory.typeParameters
+import java.lang.reflect.Modifier
+import org.eclipse.jdt.core.IType
+import org.eclipse.jdt.core.dom.IMethodBinding
+import org.eclipse.jdt.core.dom.ITypeBinding
+import org.eclipse.jdt.internal.core.BinaryType
+import org.jetbrains.kotlin.descriptors.Visibility
+import org.jetbrains.kotlin.load.java.structure.JavaAnnotation
+import org.jetbrains.kotlin.load.java.structure.JavaClass
+import org.jetbrains.kotlin.load.java.structure.JavaClassifierType
+import org.jetbrains.kotlin.load.java.structure.JavaConstructor
+import org.jetbrains.kotlin.load.java.structure.JavaField
+import org.jetbrains.kotlin.load.java.structure.JavaMethod
+import org.jetbrains.kotlin.load.java.structure.JavaType
+import org.jetbrains.kotlin.load.java.structure.JavaTypeParameter
+import org.jetbrains.kotlin.load.java.structure.JavaTypeSubstitutor
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
+import com.google.common.collect.Lists
+import org.jetbrains.kotlin.load.java.structure.JavaClass.OriginKind
 
-import java.lang.reflect.Modifier;
-import java.util.Collection;
-import java.util.List;
-
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.dom.IMethodBinding;
-import org.eclipse.jdt.core.dom.ITypeBinding;
-import org.eclipse.jdt.internal.core.BinaryType;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.descriptors.Visibility;
-import org.jetbrains.kotlin.load.java.structure.JavaAnnotation;
-import org.jetbrains.kotlin.load.java.structure.JavaClass;
-import org.jetbrains.kotlin.load.java.structure.JavaClassifierType;
-import org.jetbrains.kotlin.load.java.structure.JavaConstructor;
-import org.jetbrains.kotlin.load.java.structure.JavaField;
-import org.jetbrains.kotlin.load.java.structure.JavaMethod;
-import org.jetbrains.kotlin.load.java.structure.JavaType;
-import org.jetbrains.kotlin.load.java.structure.JavaTypeParameter;
-import org.jetbrains.kotlin.load.java.structure.JavaTypeSubstitutor;
-import org.jetbrains.kotlin.name.FqName;
-import org.jetbrains.kotlin.name.Name;
-
-import com.google.common.collect.Lists;
-
-public class EclipseJavaClass extends EclipseJavaClassifier<ITypeBinding> implements JavaClass {
-
-    public EclipseJavaClass(ITypeBinding javaElement) {
-        super(javaElement);
+public class EclipseJavaClass(javaElement: ITypeBinding) : EclipseJavaClassifier<ITypeBinding>(javaElement), JavaClass {
+    override public fun getAnnotations(): Collection<JavaAnnotation> {
+        return annotations(getBinding().getAnnotations())
     }
     
-    @Override
-    @NotNull
-    public Collection<JavaAnnotation> getAnnotations() {
-        return annotations(getBinding().getAnnotations());
+    override public fun findAnnotation(fqName: FqName): JavaAnnotation? {
+        return EclipseJavaElementUtil.findAnnotation(getBinding().getAnnotations(), fqName)
     }
     
-    @Override
-    @Nullable
-    public JavaAnnotation findAnnotation(@NotNull FqName fqName) {
-        return EclipseJavaElementUtil.findAnnotation(getBinding().getAnnotations(), fqName);
-    }
-
-    @Override
-    @NotNull
-    public Name getName() {
-        return Name.guess(getBinding().getName());
-    }
-
-    @Override
-    public boolean isAbstract() {
-        return Modifier.isAbstract(getBinding().getModifiers());
-    }
-
-    @Override
-    public boolean isStatic() {
-        return Modifier.isStatic(getBinding().getModifiers());
-    }
-
-    @Override
-    public boolean isFinal() {
-        return Modifier.isFinal(getBinding().getModifiers());
-    }
-
-    @Override
-    @NotNull
-    public Visibility getVisibility() {
-        return EclipseJavaElementUtil.getVisibility(getBinding());
-    }
-
-    @Override
-    @NotNull
-    public List<JavaTypeParameter> getTypeParameters() {
-        return typeParameters(getBinding().getTypeParameters());
-    }
-
-    @Override
-    @NotNull
-    public Collection<JavaClass> getInnerClasses() {
-        List<JavaClass> innerClasses = Lists.newArrayList();
-        for (ITypeBinding innerClass : getBinding().getDeclaredTypes()) {
-            innerClasses.add(new EclipseJavaClass(innerClass));
-        }
-        
-        return innerClasses;
-    }
-
-    @Override
-    @Nullable
-    public FqName getFqName() {
-        return new FqName(getBinding().getQualifiedName());
-    }
-
-    @Override
-    public boolean isInterface() {
-        return getBinding().isInterface();
-    }
-
-    @Override
-    public boolean isAnnotationType() {
-        return getBinding().isAnnotation();
-    }
-
-    @Override
-    public boolean isEnum() {
-        return getBinding().isEnum();
-    }
-
-    @Override
-    @Nullable
-    public JavaClass getOuterClass() {
-        ITypeBinding outerClass = getBinding().getDeclaringClass();
-        return outerClass != null ? new EclipseJavaClass(outerClass) : null;
-    }
-
-    @Override
-    @NotNull
-    public Collection<JavaClassifierType> getSupertypes() {
-        return classifierTypes(EclipseJavaElementUtil.getSuperTypesWithObject(getBinding()));
-    }
-
-    @Override
-    @NotNull
-    public Collection<JavaMethod> getMethods() {
-        return methods(getBinding().getDeclaredMethods());
-    }
-
-    @Override
-    @NotNull
-    public Collection<JavaField> getFields() {
-        return fields(getBinding().getDeclaredFields());
+    override public fun getName(): Name = Name.guess(getBinding().getName())
+    
+    override public fun isAbstract(): Boolean = Modifier.isAbstract(getBinding().getModifiers())
+    
+    override public fun isStatic(): Boolean = Modifier.isStatic(getBinding().getModifiers())
+    
+    override public fun isFinal(): Boolean = Modifier.isFinal(getBinding().getModifiers())
+    
+    override public fun getVisibility(): Visibility = EclipseJavaElementUtil.getVisibility(getBinding())
+    
+    override public fun getTypeParameters(): List<JavaTypeParameter> {
+    	return typeParameters(getBinding().getTypeParameters())
     }
     
-    @Override
-    @NotNull
-    public Collection<JavaConstructor> getConstructors() {
-        Collection<JavaConstructor> constructors = Lists.newArrayList();
-        for (IMethodBinding method : getBinding().getDeclaredMethods()) {
-            if (method.isConstructor()) {
-                constructors.add(new EclipseJavaConstructor(method));
-            }
+    override public fun getInnerClasses(): Collection<JavaClass> {
+        return getBinding().getDeclaredTypes().map { EclipseJavaClass(it) }
+    }
+    
+    override public fun getFqName(): FqName? = FqName(getBinding().getQualifiedName())
+    
+    override public fun isInterface(): Boolean = getBinding().isInterface()
+    
+    override public fun isAnnotationType(): Boolean = getBinding().isAnnotation()
+    
+    override public fun isEnum(): Boolean = getBinding().isEnum()
+    
+    override public fun getOuterClass(): JavaClass? {
+        val outerClass = getBinding().getDeclaringClass()
+        return if (outerClass != null) EclipseJavaClass(outerClass) else null
+    }
+    
+    override public fun getSupertypes(): Collection<JavaClassifierType> {
+        return classifierTypes(EclipseJavaElementUtil.getSuperTypesWithObject(getBinding()))
+    }
+    
+    override public fun getMethods(): Collection<JavaMethod> {
+        return methods(getBinding().getDeclaredMethods())
+    }
+    
+    override public fun getFields(): Collection<JavaField> {
+        return fields(getBinding().getDeclaredFields())
+    }
+    
+    override public fun getConstructors(): Collection<JavaConstructor> {
+        return getBinding().getDeclaredMethods()
+        		.filter { it.isConstructor() }
+        		.map { EclipseJavaConstructor(it) }
+    }
+    
+    override public fun getDefaultType(): JavaClassifierType {
+        return EclipseJavaClassifierType(getBinding().getTypeDeclaration())
+    }
+    
+    override public fun getOriginKind(): OriginKind {
+        val javaType = getBinding().getJavaElement() as IType
+        return when {
+            EclipseJavaElementUtil.isKotlinLightClass(javaType) -> OriginKind.KOTLIN_LIGHT_CLASS
+            javaType is BinaryType -> OriginKind.COMPILED
+            else -> OriginKind.SOURCE 
         }
-        
-        return constructors;
     }
-
-    @Override
-    @NotNull
-    public JavaClassifierType getDefaultType() {
-        return new EclipseJavaClassifierType(getBinding().getTypeDeclaration());
+    
+    override public fun createImmediateType(substitutor: JavaTypeSubstitutor): JavaType {
+        return EclipseJavaImmediateClass(this, substitutor)
     }
-
-    @Override
-    @NotNull
-    public OriginKind getOriginKind() {
-        IType javaType = (IType) getBinding().getJavaElement();
-        if (EclipseJavaElementUtil.isKotlinLightClass(javaType)) {
-            return OriginKind.KOTLIN_LIGHT_CLASS;
-        }
-        
-        if (javaType instanceof BinaryType) {
-            return OriginKind.COMPILED;
-        } else {
-            return OriginKind.SOURCE;
-        }
-    }
-
-    @Override
-    @NotNull
-    public JavaType createImmediateType(@NotNull JavaTypeSubstitutor substitutor) {
-        return new EclipseJavaImmediateClass(this, substitutor);
-    }
-
 }
--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/lang/java/structure/EclipseJavaTypeParameter.kt
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/lang/java/structure/EclipseJavaTypeParameter.kt
@@ -1,86 +1,57 @@
 /*******************************************************************************
- * Copyright 2000-2014 JetBrains s.r.o.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *  
- * http://www.apache.org/licenses/LICENSE-2.0
- *   
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *******************************************************************************/
-package org.jetbrains.kotlin.core.resolve.lang.java.structure;
+* Copyright 2000-2014 JetBrains s.r.o.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+package org.jetbrains.kotlin.core.resolve.lang.java.structure
 
-import java.util.Collection;
-import java.util.List;
+import org.eclipse.jdt.core.dom.IMethodBinding
+import org.eclipse.jdt.core.dom.ITypeBinding
+import org.jetbrains.kotlin.load.java.structure.JavaClassifierType
+import org.jetbrains.kotlin.load.java.structure.JavaType
+import org.jetbrains.kotlin.load.java.structure.JavaTypeParameter
+import org.jetbrains.kotlin.load.java.structure.JavaTypeParameterListOwner
+import org.jetbrains.kotlin.load.java.structure.JavaTypeProvider
+import org.jetbrains.kotlin.name.Name
+import com.google.common.collect.Lists
 
-import org.eclipse.jdt.core.dom.IMethodBinding;
-import org.eclipse.jdt.core.dom.ITypeBinding;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.load.java.structure.JavaClassifierType;
-import org.jetbrains.kotlin.load.java.structure.JavaType;
-import org.jetbrains.kotlin.load.java.structure.JavaTypeParameter;
-import org.jetbrains.kotlin.load.java.structure.JavaTypeParameterListOwner;
-import org.jetbrains.kotlin.load.java.structure.JavaTypeProvider;
-import org.jetbrains.kotlin.name.Name;
-
-import com.google.common.collect.Lists;
-
-public class EclipseJavaTypeParameter extends EclipseJavaClassifier<ITypeBinding> implements JavaTypeParameter {
-
-    protected EclipseJavaTypeParameter(ITypeBinding binding) {
-        super(binding);
+public class EclipseJavaTypeParameter(binding: ITypeBinding) : EclipseJavaClassifier<ITypeBinding>(binding), JavaTypeParameter {
+    override public fun getName(): Name = Name.identifier(getBinding().getName())
+    
+    override public fun getUpperBounds(): Collection<JavaClassifierType> {
+        return getBinding().getTypeBounds().map { EclipseJavaClassifierType(it) }
     }
-
-    @Override
-    @NotNull
-    public Name getName() {
-        return Name.identifier(getBinding().getName());
-    }
-
-    @Override
-    @NotNull
-    public Collection<JavaClassifierType> getUpperBounds() {
-        List<JavaClassifierType> bounds = Lists.newArrayList();
-        for (ITypeBinding bound : getBinding().getTypeBounds()) {
-            bounds.add(new EclipseJavaClassifierType(bound));
-        }
-        
-        return bounds;
-    }
-
-    @Override
-    @Nullable
-    public JavaTypeParameterListOwner getOwner() {
-        IMethodBinding methodOwner = getBinding().getDeclaringMethod();
+    
+    override public fun getOwner(): JavaTypeParameterListOwner? {
+        val methodOwner = getBinding().getDeclaringMethod()
         if (methodOwner != null) {
-            return new EclipseJavaMethod(methodOwner);
+            return EclipseJavaMethod(methodOwner)
         }
         
-        ITypeBinding typeOwner = getBinding().getDeclaringClass();
+        val typeOwner = getBinding().getDeclaringClass()
         if (typeOwner != null) {
-            return new EclipseJavaClass(typeOwner);
+            return EclipseJavaClass(typeOwner)
         }
         
-        return null;
+        return null
     }
-
-    @Override
-    @NotNull
-    public JavaType getType() {
-        return EclipseJavaType.create(getBinding().getTypeDeclaration());
+    
+    override public fun getType(): JavaType {
+        return EclipseJavaType.create(getBinding().getTypeDeclaration())
     }
-
-    @Override
-    @NotNull
-    public JavaTypeProvider getTypeProvider() {
-        return new EclipseJavaTypeProvider(getBinding().getJavaElement().getJavaProject());
+    
+    override public fun getTypeProvider(): JavaTypeProvider {
+        return EclipseJavaTypeProvider(getBinding().getJavaElement().getJavaProject())
     }
-
 }
