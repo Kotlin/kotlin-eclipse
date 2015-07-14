6e4ab2d761bef7619837ed642d3829e750b2d6a6
Dmitry Tishchenko <dmitry.tishchenko@jetbrains.com>
2015-07-14 14:27:26 +0300

    Refactoring: List selectioner rewritten in more functional style.

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/selection/handlers/KotlinListSelectionHandler.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/selection/handlers/KotlinListSelectionHandler.kt
@@ -11,48 +11,42 @@ import org.jetbrains.kotlin.psi.JetParameter
 import org.jetbrains.kotlin.psi.JetTypeParameter
 import org.jetbrains.kotlin.psi.JetTypeProjection
 
-public class KotlinListSelectionHandler: KotlinElementSelectionHandler {
-	override fun canSelect(enclosingElement: PsiElement) =
-	enclosingElement is JetParameterList ||
-	enclosingElement is JetValueArgumentList ||
-	enclosingElement is JetTypeParameterList ||
-	enclosingElement is JetTypeArgumentList
+public class KotlinListSelectionHandler: KotlinDefaultSelectionHandler() {
+    override fun canSelect(enclosingElement: PsiElement) =
+	    enclosingElement is JetParameterList ||
+	    enclosingElement is JetValueArgumentList ||
+	    enclosingElement is JetTypeParameterList ||
+	    enclosingElement is JetTypeArgumentList
 
-	override fun selectEnclosing(enclosingElement: PsiElement, selectedRange: TextRange): TextRange {
-		val elementRange = enclosingElement.getTextRange();
-		//delete parentheses
-		val resultRange = TextRange(elementRange.getStartOffset()+1, elementRange.getEndOffset()-1)
-		if (resultRange.equals(selectedRange) || !resultRange.contains(selectedRange)) {
-			return KotlinElementSelectioner.getInstance().selectEnclosing(enclosingElement.getParent(), selectedRange)
-		}
-		return resultRange
-	}
+    override fun selectEnclosing(enclosingElement: PsiElement, selectedRange: TextRange): TextRange {
+        val elementRange = enclosingElement.getTextRange();
+        //delete parentheses
+        val resultRange = TextRange(elementRange.getStartOffset() + 1, elementRange.getEndOffset()-1)
+        if (resultRange == selectedRange || selectedRange !in resultRange) {
+            return KotlinElementSelectioner.selectEnclosing(enclosingElement.getParent(), selectedRange)
+        }
+        return resultRange
+    }
 
-	private fun isElementOfListPartType(element: PsiElement): Boolean =
-	(element is JetValueArgument || element is JetParameter ||
-	element is JetTypeParameter ||	element is JetTypeProjection)
+    private fun isElementOfListPartType(element: PsiElement): Boolean =
+	    element is JetValueArgument || element is JetParameter ||
+	    element is JetTypeParameter || element is JetTypeProjection
 
-	override fun selectPrevious(enclosingElement: PsiElement, selectionCandidate: PsiElement, selectedRange: TextRange): TextRange {
-		var currentSelectionCandidate: PsiElement? = selectionCandidate
-		while (currentSelectionCandidate != null && !isElementOfListPartType(currentSelectionCandidate)) {
-			currentSelectionCandidate = currentSelectionCandidate.getPrevSibling()
-		}
-		if (currentSelectionCandidate == null) {
-			return selectEnclosing(enclosingElement, selectedRange)
-		}
-		val elementRange = currentSelectionCandidate.getTextRange();
-		return TextRange(elementRange.getStartOffset(), selectedRange.getEndOffset())
-	}
+    override fun selectPrevious(enclosingElement: PsiElement, selectionCandidate: PsiElement, selectedRange: TextRange): TextRange {
+        var elementToSelect = selectionCandidate.siblings(forward = false, withItself = true)
+            .firstOrNull { isElementOfListPartType(it) }
+        if (elementToSelect == null) {
+            return selectEnclosing(enclosingElement, selectedRange)
+        }
+        return selectionWithElementAppendedToBeginning(selectedRange, elementToSelect)
+    }
 
-	override fun selectNext(enclosingElement: PsiElement, selectionCandidate: PsiElement, selectedRange: TextRange): TextRange {
-		var currentSelectionCandidate: PsiElement? = selectionCandidate
-		while (currentSelectionCandidate != null && !isElementOfListPartType(currentSelectionCandidate)) {
-			currentSelectionCandidate = currentSelectionCandidate.getNextSibling()
-		}
-		if (currentSelectionCandidate == null) {
-			return selectEnclosing(enclosingElement, selectedRange)
-		}
-		val elementRange = currentSelectionCandidate.getTextRange();
-		return TextRange(selectedRange.getStartOffset(), elementRange.getEndOffset())
-	}
+    override fun selectNext(enclosingElement: PsiElement, selectionCandidate: PsiElement, selectedRange: TextRange): TextRange {
+        var elementToSelect = selectionCandidate.siblings(forward = true, withItself = true)
+            .firstOrNull { isElementOfListPartType(it) }
+        if (elementToSelect == null) {
+            return selectEnclosing(enclosingElement, selectedRange)
+        }
+        return selectionWithElementAppendedToEnd(selectedRange, elementToSelect)
+    }
 }
--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/selection/handlers/PsiUtils.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/selection/handlers/PsiUtils.kt
@@ -8,6 +8,11 @@ public val PsiElement.allChildren: PsiChildRange
         val first = getFirstChild()
         return if (first != null) PsiChildRange(first, getLastChild()) else PsiChildRange.EMPTY
     }
+public fun PsiElement.siblings(forward: Boolean = true, withItself: Boolean = true): Sequence<PsiElement> {
+    val stepFun = if (forward) { e: PsiElement -> e.getNextSibling() } else { e: PsiElement -> e.getPrevSibling() }
+    val sequence = sequence(this, stepFun)
+    return if (withItself) sequence else sequence.drop(1)
+}
 public val PsiElement.startOffset: Int
     get() = getTextRange().getStartOffset()
 
