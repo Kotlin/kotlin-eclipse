53af6fdcdd0075b364dc61915b02e08a95190609
Dmitry Tishchenko <dmitry.tishchenko@jetbrains.com>
2015-07-14 13:42:54 +0300

    Refactoring: Kotlin Declaration selectioner rewritten to more functional style

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/selection/handlers/KotlinDeclarationSelectionHandler.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/selection/handlers/KotlinDeclarationSelectionHandler.kt
@@ -6,52 +6,53 @@ import org.jetbrains.kotlin.psi.JetDeclaration
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiWhiteSpace
 
-public class KotlinDeclarationSelectionHandler: KotlinElementSelectionHandler {
-	override fun canSelect(enclosingElement: PsiElement)
-		= enclosingElement is JetDeclaration
-	
-	override fun selectNext(enclosingElement: PsiElement, selectionCandidate: PsiElement, selectedRange: TextRange): TextRange {
-		val bodyRange = getBodyRange(enclosingElement)
-		if (bodyRange.contains(selectionCandidate.getTextRange())) {
-			return selectEnclosing(enclosingElement, selectedRange)
-		}
-		if (selectionCandidate is PsiComment) {
-			return TextRange(selectedRange.getStartOffset(), selectionCandidate.getTextRange().getEndOffset())
-		}
-		return enclosingElement.getTextRange()
-	}
-	
-	override fun selectPrevious(enclosingElement: PsiElement, selectionCandidate: PsiElement, selectedRange: TextRange): TextRange {
-		val bodyRange = getBodyRange(enclosingElement)
-		if (bodyRange.contains(selectionCandidate.getTextRange())) {
-			return selectEnclosing(enclosingElement, selectedRange)
-		}
-		if (selectionCandidate is PsiComment) {
-			return TextRange(selectionCandidate.getTextRange().getStartOffset(), selectedRange.getEndOffset())
-		}
-		return enclosingElement.getTextRange()
-	}
-	
-	override fun selectEnclosing(enclosingElement: PsiElement, selectedRange: TextRange): TextRange {
-		val bodyRange = getBodyRange(enclosingElement)
-		if (!bodyRange.contains(selectedRange) || bodyRange.equals(selectedRange)) {
-			return enclosingElement.getTextRange()
-		}
-		return bodyRange;
-	}
-	
-	private fun getBodyRange(enclosingElement: PsiElement): TextRange {
-		var firstNonCommentChild = enclosingElement.getFirstChild()
-		while (firstNonCommentChild!=null && (firstNonCommentChild is PsiWhiteSpace || firstNonCommentChild is PsiComment)) {
-			firstNonCommentChild = firstNonCommentChild.getNextSibling()
-		}
-		var lastNonCommentChild = enclosingElement.getLastChild()
-		while (lastNonCommentChild!=null && (lastNonCommentChild is PsiWhiteSpace || lastNonCommentChild is PsiComment)) {
-			lastNonCommentChild = lastNonCommentChild.getPrevSibling()
-		}
-		if (firstNonCommentChild == null || lastNonCommentChild == null) {
-			return enclosingElement.getTextRange() //WHY SO SERIOUS???
-		}
-		return TextRange(firstNonCommentChild.getTextRange().getStartOffset(), lastNonCommentChild.getTextRange().getEndOffset())
-	}
+public class KotlinDeclarationSelectionHandler: KotlinDefaultSelectionHandler() {
+    override fun canSelect(enclosingElement: PsiElement)
+    = enclosingElement is JetDeclaration
+
+    override fun selectNext(enclosingElement: PsiElement, selectionCandidate: PsiElement, selectedRange: TextRange): TextRange {
+        val bodyRange = getBodyRange(enclosingElement)
+        if (selectionCandidate.getTextRange() in bodyRange) {
+            return selectEnclosing(enclosingElement, selectedRange)
+        }
+        if (selectionCandidate is PsiComment) {
+            return selectionWithElementAppendedToEnd(selectedRange, selectionCandidate)
+        }
+        return enclosingElement.getTextRange()
+    }
+
+    override fun selectPrevious(enclosingElement: PsiElement, selectionCandidate: PsiElement, selectedRange: TextRange): TextRange {
+        val bodyRange = getBodyRange(enclosingElement)
+        if (selectionCandidate.getTextRange() in bodyRange) {
+            return selectEnclosing(enclosingElement, selectedRange)
+        }
+        if (selectionCandidate is PsiComment) {
+            return selectionWithElementAppendedToBeginning(selectedRange, selectionCandidate)
+        }
+        return enclosingElement.getTextRange()
+    }
+
+    override fun selectEnclosing(enclosingElement: PsiElement, selectedRange: TextRange): TextRange {
+        val bodyRange = getBodyRange(enclosingElement)
+        if (selectedRange !in bodyRange || bodyRange == selectedRange) {
+            return enclosingElement.getTextRange()
+        }
+        return bodyRange;
+    }
+
+    private fun getBodyRange(enclosingElement: PsiElement): TextRange {
+
+        val firstNonCommentChild = enclosingElement.allChildren
+            .firstOrNull { it !is PsiWhiteSpace && it !is PsiComment }
+            ?: enclosingElement.getFirstChild()
+
+        var lastNonCommentChild = enclosingElement.allChildren
+            .toList()
+            .reverse()
+            .asSequence()
+            .firstOrNull { it !is PsiWhiteSpace && it !is PsiComment }
+            ?: enclosingElement.getLastChild()
+
+        return TextRange(firstNonCommentChild.startOffset, lastNonCommentChild.endOffset)
+    }
 }
