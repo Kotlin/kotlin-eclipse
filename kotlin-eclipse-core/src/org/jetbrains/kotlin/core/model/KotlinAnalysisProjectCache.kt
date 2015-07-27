2d460b18983d33ebc8ec12cf4e7420437296d4bf
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-07-27 14:23:15 +0300

    J2K KotlinAnalysisProjectCache: rewrite

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/model/KotlinAnalysisProjectCache.kt
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/model/KotlinAnalysisProjectCache.kt
@@ -1,45 +1,39 @@
-package org.jetbrains.kotlin.core.model;
+package org.jetbrains.kotlin.core.model
 
-import org.eclipse.jdt.core.IJavaProject;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.analyzer.AnalysisResult;
-import org.jetbrains.kotlin.core.resolve.KotlinAnalyzer;
-import org.jetbrains.kotlin.core.utils.ProjectUtils;
+import org.eclipse.jdt.core.IJavaProject
+import org.jetbrains.kotlin.analyzer.AnalysisResult
+import org.jetbrains.kotlin.core.resolve.KotlinAnalyzer
+import org.jetbrains.kotlin.core.utils.ProjectUtils
+import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.project.Project
+import kotlin.platform.platformStatic
 
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.project.Project;
-
-public class KotlinAnalysisProjectCache {
-    private final IJavaProject javaProject;
-    
-    private AnalysisResult cachedAnalysisResult = null;
-    private final Object cacheLock = new Object();
-    
-    public KotlinAnalysisProjectCache(@NotNull IJavaProject javaProject) {
-        this.javaProject = javaProject;
-    }
-    
-    public void resetCache() {
-        synchronized (cacheLock) {
-            cachedAnalysisResult = null;
+public class KotlinAnalysisProjectCache(val javaProject: IJavaProject) {
+    companion object {
+        @platformStatic
+        public fun getInstance(javaProject: IJavaProject): KotlinAnalysisProjectCache {
+            val ideaProject = KotlinEnvironment.getEnvironment(javaProject).getProject()
+            return ServiceManager.getService(ideaProject, javaClass<KotlinAnalysisProjectCache>())
         }
     }
     
-    @NotNull
-    public AnalysisResult getAnalysisResult() {
+    private var cachedAnalysisResult: AnalysisResult? = null
+    private val cacheLock = Object()
+    
+    public fun resetCache() {
+        synchronized (cacheLock) {
+            cachedAnalysisResult = null
+        }
+    }
+    
+    public fun getAnalysisResult(): AnalysisResult {
         synchronized (cacheLock) {
             if (cachedAnalysisResult == null) {
-                KotlinEnvironment environment = KotlinEnvironment.getEnvironment(javaProject);
-                cachedAnalysisResult = KotlinAnalyzer.analyzeFiles(javaProject, environment, ProjectUtils.getSourceFiles(javaProject.getProject()));
+                val environment = KotlinEnvironment.getEnvironment(javaProject)
+                cachedAnalysisResult = KotlinAnalyzer.analyzeFiles(javaProject, environment, ProjectUtils.getSourceFiles(javaProject.getProject()))
             }
-            
-            return cachedAnalysisResult;
+                
+            return cachedAnalysisResult!!
         }
-    }
-    
-    @NotNull
-    public static KotlinAnalysisProjectCache getInstance(@NotNull IJavaProject javaProject) {
-        Project ideaProject = KotlinEnvironment.getEnvironment(javaProject).getProject();
-        return ServiceManager.getService(ideaProject, KotlinAnalysisProjectCache.class);
     }
 }
