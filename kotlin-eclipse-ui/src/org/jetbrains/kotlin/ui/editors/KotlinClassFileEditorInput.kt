8351c48a5e7bfcb8ee480322d6ca4ef917ede43a
Dmitry Tishchenko <dmitry.tishchenko@jetbrains.com>
2015-08-31 14:08:03 +0300

    Archived sources are compared by full paths now.

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/KotlinClassFileEditorInput.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/KotlinClassFileEditorInput.kt
@@ -6,22 +6,41 @@ import org.eclipse.jdt.core.IJavaProject
 import org.eclipse.jdt.core.JavaModelException
 import org.eclipse.jdt.internal.core.BufferManager
 import org.eclipse.jdt.internal.ui.javaeditor.InternalClassFileEditorInput
+import org.eclipse.jdt.internal.compiler.env.IBinaryType
+import org.jetbrains.kotlin.core.resolve.KotlinSourceIndex
+import org.eclipse.jdt.internal.core.PackageFragment
+import org.eclipse.jdt.internal.core.ClassFile
+import org.eclipse.jdt.internal.core.BinaryType
 
 public class KotlinClassFileEditorInput(classFile:IClassFile, private val javaProject:IJavaProject?):InternalClassFileEditorInput(classFile) {
 
     private fun compareSources(another:KotlinClassFileEditorInput):Boolean {
         try {
-            return getClassFile().getSource() == another.getClassFile().getSource()
-        }
-        catch (e:JavaModelException) {
+            val sourceShortName = getSourceShortName() ?: return false
+            val anotherSourceShortName = another.getSourceShortName() ?: return false
+            return when {
+                sourceShortName != anotherSourceShortName -> false
+                getClassFile().getParent() == another.getClassFile().getParent() -> true
+                else -> resolveShortSourceName(sourceShortName) == another.resolveShortSourceName(anotherSourceShortName)
+            }
+        } catch (e:JavaModelException) {
             return false
         }
     }
 
     override public fun equals(other: Any?):Boolean =
-        when {
-            super.equals(other) -> true
-            other is KotlinClassFileEditorInput -> compareSources(other)
-            else -> false
-        }
+            when {
+                super.equals(other) -> true
+                other is KotlinClassFileEditorInput -> compareSources(other)
+                else -> false
+            }
+
+    private fun getSourceShortName() =
+            (getClassFile().getType() as? BinaryType)?.getSourceFileName(null)?.toString()
+
+    private fun resolveShortSourceName(simpleSourceFileName: String): String {
+        val index = KotlinSourceIndex.getInstance(javaProject);
+        val packageFragment = getClassFile().getParent() as PackageFragment;
+        return index.resolvePath(packageFragment, simpleSourceFileName);
+    }
 }
