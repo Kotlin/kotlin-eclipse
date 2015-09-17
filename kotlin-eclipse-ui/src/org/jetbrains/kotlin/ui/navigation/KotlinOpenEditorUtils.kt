e65aed58dd19324b023ca488eac29c56bf669883
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-09-17 14:00:42 +0300

    Minor. Reformat file

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/navigation/KotlinOpenEditorUtils.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/navigation/KotlinOpenEditorUtils.kt
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *******************************************************************************/
+* Copyright 2000-2015 JetBrains s.r.o.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
 package org.jetbrains.kotlin.ui.navigation
 
 import org.jetbrains.kotlin.psi.JetVisitorVoid
@@ -49,100 +49,101 @@ import org.jetbrains.kotlin.core.asJava.equalsJvmSignature
 import org.jetbrains.kotlin.core.asJava.getDeclaringTypeFqName
 
 fun findKotlinDeclaration(element: IJavaElement, jetFile: JetFile): JetElement? {
-	val result = ArrayList<JetElement>()
-	val visitor = makeVisitor(element, result)
-	if (visitor != null) {
-		jetFile.acceptChildren(visitor)
-	}
+    val result = ArrayList<JetElement>()
+    val visitor = makeVisitor(element, result)
+    if (visitor != null) {
+        jetFile.acceptChildren(visitor)
+    }
 
-	return result.firstOrNull()
+    return result.firstOrNull()
 }
 
 fun makeVisitor(element: IJavaElement, result: MutableList<JetElement>): JetVisitorVoid? {
-	return when (element) {
-		is IType -> object : JetAllVisitor() {
-			override fun visitClassOrObject(jetClassOrObject: JetClassOrObject) {
-				if (jetClassOrObject.getFqName() == element.getFqName()) {
-					result.add(jetClassOrObject)
-					return
-				}
-				
-				jetClassOrObject.acceptChildren(this)
-			}
-		}
-		is IField -> object: JetAllVisitor() {
-			override fun visitObjectDeclaration(declaration: JetObjectDeclaration) {
-				visitExplicitDeclaration(declaration)
-				declaration.acceptChildren(this)
-			}
-			
-			override fun visitEnumEntry(enumEntry: JetEnumEntry) {
-				visitExplicitDeclaration(enumEntry)
-			}
-            
+    return when (element) {
+        is IType -> object : JetAllVisitor() {
+            override fun visitClassOrObject(jetClassOrObject: JetClassOrObject) {
+                if (jetClassOrObject.getFqName() == element.getFqName()) {
+                    result.add(jetClassOrObject)
+                    return
+                }
+
+                jetClassOrObject.acceptChildren(this)
+            }
+        }
+        
+        is IField -> object: JetAllVisitor() {
+            override fun visitObjectDeclaration(declaration: JetObjectDeclaration) {
+                visitExplicitDeclaration(declaration)
+                declaration.acceptChildren(this)
+            }
+
+            override fun visitEnumEntry(enumEntry: JetEnumEntry) {
+                visitExplicitDeclaration(enumEntry)
+            }
+
             override fun visitProperty(property: JetProperty) {
                 visitExplicitDeclaration(property)
             }
-			
-			fun visitExplicitDeclaration(declaration: JetDeclaration) {
-				if (equalsJvmSignature(declaration, element)) {
-					result.add(declaration)
-				}
-			}
-		}
-		is IMethod -> 
-			object : JetAllVisitor() {
-				override fun visitNamedFunction(function: JetNamedFunction) {
-					visitExplicitDeclaration(function)
-				}
-			
-				override fun visitProperty(property: JetProperty) {
-					visitExplicitDeclaration(property)
-					property.acceptChildren(this)
-				}
-			
-				override fun visitPropertyAccessor(accessor: JetPropertyAccessor) {
-					visitExplicitDeclaration(accessor)
-				}
-			
-				override fun visitSecondaryConstructor(constructor: JetSecondaryConstructor) {
-					visitExplicitDeclaration(constructor)
-				}
-			
-				override fun visitPrimaryConstructor(constructor: JetPrimaryConstructor) {
-					visitExplicitDeclaration(constructor)
-				}
-			
-//				Check primary constructor when there are no secondary constructors
-				override fun visitClass(jetClass: JetClass) {
-					if (equalsJvmSignature(jetClass, element) && (jetClass.getFqName() == element.getDeclaringType().getFqName())) {
-						result.add(jetClass)
-						return
-					}
-					
-					jetClass.acceptChildren(this)
-				}
-			
-				fun visitExplicitDeclaration(declaration: JetDeclaration) {
-					if (equalsJvmSignature(declaration, element) && equalsDeclaringTypes(declaration, element)) {
-						result.add(declaration)
-					}
-				}
-			}
-		else -> null
-	}
+
+            fun visitExplicitDeclaration(declaration: JetDeclaration) {
+                if (equalsJvmSignature(declaration, element)) {
+                    result.add(declaration)
+                }
+            }
+        }
+        
+        is IMethod -> object : JetAllVisitor() {
+            override fun visitNamedFunction(function: JetNamedFunction) {
+                visitExplicitDeclaration(function)
+            }
+
+            override fun visitProperty(property: JetProperty) {
+                visitExplicitDeclaration(property)
+                property.acceptChildren(this)
+            }
+
+            override fun visitPropertyAccessor(accessor: JetPropertyAccessor) {
+                visitExplicitDeclaration(accessor)
+            }
+
+            override fun visitSecondaryConstructor(constructor: JetSecondaryConstructor) {
+                visitExplicitDeclaration(constructor)
+            }
+
+            override fun visitPrimaryConstructor(constructor: JetPrimaryConstructor) {
+                visitExplicitDeclaration(constructor)
+            }
+
+//          Check primary constructor when there are no secondary constructors
+            override fun visitClass(jetClass: JetClass) {
+                if (equalsJvmSignature(jetClass, element) && (jetClass.getFqName() == element.getDeclaringType().getFqName())) {
+                    result.add(jetClass)
+                    return
+                }
+
+                jetClass.acceptChildren(this)
+            }
+
+            fun visitExplicitDeclaration(declaration: JetDeclaration) {
+                if (equalsJvmSignature(declaration, element) && equalsDeclaringTypes(declaration, element)) {
+                    result.add(declaration)
+                }
+            }
+        }
+        else -> null
+    }
 }
 
 fun IType.getFqName(): FqName {
-	return FqName(this.getFullyQualifiedName('.'))
+    return FqName(this.getFullyQualifiedName('.'))
 }
 
 fun equalsDeclaringTypes(jetElement: JetElement, javaMember: IMember): Boolean  {
-	return getDeclaringTypeFqName(jetElement).any { it == javaMember.getDeclaringType().getFqName() }
+    return getDeclaringTypeFqName(jetElement).any { it == javaMember.getDeclaringType().getFqName() }
 }
 
 open class JetAllVisitor() : JetVisitorVoid() {
-	override fun visitElement(element: PsiElement) {
-		element.acceptChildren(this)
-	}
+    override fun visitElement(element: PsiElement) {
+        element.acceptChildren(this)
+    }
 }
