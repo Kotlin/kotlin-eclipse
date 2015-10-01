28103d92083d88cac0266e4b019b3fc293777754
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-10-01 16:18:18 +0300

    Initial support of rename refactoring

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/refactorings/rename/KotlinRenameAction.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/refactorings/rename/KotlinRenameAction.kt
@@ -53,6 +53,12 @@ import org.eclipse.core.runtime.IProgressMonitor
 import org.eclipse.text.edits.UndoEdit
 import org.eclipse.jdt.core.compiler.CharOperation
 import org.eclipse.jdt.internal.compiler.env.ICompilationUnit as ContentProviderCompilationUnit
+import org.jetbrains.kotlin.psi.JetElement
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.psi.JetDeclaration
+import org.jetbrains.kotlin.core.model.toLightElements
+import org.eclipse.jdt.core.IJavaProject
+import org.jetbrains.kotlin.core.log.KotlinLogger
 
 public class KotlinRenameAction(val editor: KotlinFileEditor) : SelectionDispatchAction(editor.getSite()) {
     init {
@@ -64,24 +70,48 @@ public class KotlinRenameAction(val editor: KotlinFileEditor) : SelectionDispatc
     }
     
     override fun run(selection: ITextSelection) {
-        val psiElement = EditorUtil.getPsiElement(editor, selection.getOffset())
-        if (psiElement == null) return
+        val jetElement = getJetElement(selection)
+        if (jetElement == null) return
         
-        val expression = getReferenceExpression(psiElement)
-        if (expression == null) return
-        
-        val sourceElements = createReference(expression).resolveToSourceElements()
-        val javaElements = sourceElementsToLightElements(sourceElements, editor.javaProject!!)
+        val javaElements = resolveToJavaElements(jetElement, editor.javaProject!!)
         if (javaElements.isEmpty()) return
         
-        beginRenameRefactoring(javaElements[0], psiElement, editor)
+        if (javaElements.size() > 1) {
+            KotlinLogger.logWarning("There are more than one (${javaElements.size()}) java elements for $jetElement")
+        }
+        
+        beginRenameRefactoring(javaElements[0], jetElement, editor)
+    }
+    
+    private fun getJetElement(selection: ITextSelection): JetElement? {
+        val psiElement = EditorUtil.getPsiElement(editor, selection.getOffset())
+        if (psiElement != null) {
+            return PsiTreeUtil.getNonStrictParentOfType(psiElement, JetElement::class.java)
+        }
+        
+        return null
     }
 }
 
-fun beginRenameRefactoring(javaElement: IJavaElement, psiElement: PsiElement, editor: KotlinFileEditor) {
+fun resolveToJavaElements(jetElement: JetElement, javaProject: IJavaProject): List<IJavaElement> {
+    return when (jetElement) {
+        is JetDeclaration -> jetElement.toLightElements(javaProject)
+        else -> {
+            val referenceExpression = getReferenceExpression(jetElement)
+            if (referenceExpression == null) return emptyList()
+            
+            val sourceElements = createReference(referenceExpression).resolveToSourceElements()
+            sourceElementsToLightElements(sourceElements, javaProject)
+        }
+    }
+}
+
+fun beginRenameRefactoring(javaElement: IJavaElement, jetElement: JetElement, editor: KotlinFileEditor) {
     val linkedPositionGroup = LinkedPositionGroup()
-    val offsetInDocument = psiElement.getTextDocumentOffset(editor.document)
-    linkedPositionGroup.addPosition(LinkedPosition(editor.document, offsetInDocument, psiElement.getTextLength()))
+    val offsetInDocument = jetElement.getTextDocumentOffset(editor.document)
+    
+    val position = LinkedPosition(editor.document, offsetInDocument, jetElement.getTextLength())
+    linkedPositionGroup.addPosition(position)
     
     val linkedModeModel = LinkedModeModel()
     linkedModeModel.addGroup(linkedPositionGroup)
@@ -90,7 +120,7 @@ fun beginRenameRefactoring(javaElement: IJavaElement, psiElement: PsiElement, ed
     linkedModeModel.addLinkingListener(object : ILinkedModeListener {
         override fun left(model: LinkedModeModel, flags: Int) {
             if ((flags and ILinkedModeListener.UPDATE_CARET) != 0) {
-                doRename(javaElement, editor)
+                doRename(javaElement, jetElement, position.getContent(), editor)
             }
         }
         
@@ -107,62 +137,16 @@ fun beginRenameRefactoring(javaElement: IJavaElement, psiElement: PsiElement, ed
     ui.enter()
 }
 
-fun doRename(javaElement: IJavaElement, editor: KotlinFileEditor) {
-    val contributionId = IJavaRefactorings.RENAME_TYPE
-    val descriptor = RefactoringCore.getRefactoringContribution(contributionId).createDescriptor() as RenameJavaElementDescriptor
-    descriptor.setJavaElement(javaElement)
-    descriptor.setNewName("${javaElement.getElementName()}K")
-    descriptor.setUpdateReferences(true)
+fun doRename(javaElement: IJavaElement, jetElement: JetElement, newName: String, editor: KotlinFileEditor) {
+//    val contributionId = IJavaRefactorings.RENAME_TYPE
+//    val descriptor = RefactoringCore.getRefactoringContribution(contributionId).createDescriptor() as RenameJavaElementDescriptor
+//    descriptor.setJavaElement(javaElement)
+//    descriptor.setNewName(newName)
+//    descriptor.setUpdateReferences(true)
     
-    val kotlinLightType = KotlinLightType(javaElement as IType, editor)
-    val renameSupport = RenameSupport.create(kotlinLightType, "${javaElement.getElementName()}K", RenameSupport.UPDATE_REFERENCES)
+    val kotlinLightType = KotlinLightType(javaElement as IType, jetElement, editor)
+    val renameSupport = RenameSupport.create(kotlinLightType, newName, RenameSupport.UPDATE_REFERENCES)
     with(editor.getSite()) {
         renameSupport.perform(getShell(), getWorkbenchWindow())
     }
-}
-
-class KotlinLightType(javaElement: IType, val editor: KotlinFileEditor) : IType by javaElement {
-    override fun getCompilationUnit(): ICompilationUnit {
-        val compilationUnit = EditorUtility.getEditorInputJavaElement(editor, false) as CompilationUnit
-        return KotlinLightCompilationUnit(editor.getFile()!!, compilationUnit)
-    }
-    
-    override fun getMethods(): Array<out IMethod> {
-        return emptyArray()
-    }
-
-    override fun getNameRange(): ISourceRange {
-        return object : ISourceRange {
-            override fun getLength(): Int {
-                return 0
-            }
-            
-            override fun getOffset(): Int {
-                return 0
-            }
-        }
-    }
-
-    override fun getPrimaryElement(): IJavaElement? = this
-
-    override fun isBinary(): Boolean = false
-
-    override fun isReadOnly(): Boolean = false
-}
-
-class KotlinLightCompilationUnit(val file: IFile, compilationUnit: ICompilationUnit) : ICompilationUnit by compilationUnit,
-       ContentProviderCompilationUnit {
-    override fun getFileName(): CharArray? = CharOperation.NO_CHAR
-    
-    override fun getContents(): CharArray? = CharOperation.NO_CHAR
-    
-    override fun ignoreOptionalProblems(): Boolean = true
-    
-    override fun getMainTypeName(): CharArray? = CharOperation.NO_CHAR
-    
-    override fun getPackageName(): Array<out CharArray>? {
-        return null
-    }
-
-    override fun getResource(): IResource = file
 }
--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/refactorings/rename/KotlinTypeRenameParticipant.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/refactorings/rename/KotlinTypeRenameParticipant.kt
@@ -21,25 +21,85 @@ import org.eclipse.core.runtime.IProgressMonitor
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext
 import org.eclipse.ltk.core.refactoring.RefactoringStatus
 import org.eclipse.ltk.core.refactoring.Change
+import kotlin.properties.Delegates
+import org.eclipse.jdt.core.IType
+import java.util.ArrayList
+import org.eclipse.ltk.core.refactoring.CompositeChange
+import org.jetbrains.kotlin.ui.search.KotlinQueryParticipant
+import org.eclipse.search.ui.text.Match
+import org.eclipse.jdt.ui.search.ElementQuerySpecification
+import org.eclipse.jdt.core.search.IJavaSearchConstants
+import org.eclipse.jdt.internal.ui.search.JavaSearchScopeFactory
+import org.eclipse.core.runtime.NullProgressMonitor
+import org.jetbrains.kotlin.ui.search.KotlinElementMatch
+import org.jetbrains.kotlin.core.builder.KotlinPsiManager
+import org.jetbrains.kotlin.eclipse.ui.utils.EditorUtil
+import org.eclipse.ltk.core.refactoring.TextFileChange
+import org.jetbrains.kotlin.eclipse.ui.utils.getTextDocumentOffset
+import org.eclipse.text.edits.ReplaceEdit
+import org.eclipse.ltk.core.refactoring.participants.RefactoringProcessor
+import org.eclipse.ltk.core.refactoring.participants.RefactoringArguments
+import org.eclipse.ltk.core.refactoring.participants.RenameArguments
 
 public class KotlinTypeRenameParticipant : RenameParticipant() {
-    override fun initialize(element: Any?): Boolean {
-        println("cool")
+    lateinit var element: IType
+    lateinit var newName: String
+    
+    val changes = arrayListOf<Change>()
+        
+    override fun initialize(element: Any): Boolean {
+        this.element = if (element is KotlinLightType) element.originElement else element as IType
+        changes.clear()
         return true
     }
     
-    override fun checkConditions(pm: IProgressMonitor?, context: CheckConditionsContext?): RefactoringStatus? {
-        println("cool")
-        return null
+    override fun initialize(processor: RefactoringProcessor, element: Any?, arguments: RefactoringArguments): Boolean {
+        if (arguments is RenameArguments) {
+            newName = arguments.getNewName()
+        }
+        
+        return super.initialize(processor, element, arguments)
     }
     
-    override fun getName(): String? {
-        println("cool")
-        return null
+    override fun checkConditions(pm: IProgressMonitor, context: CheckConditionsContext): RefactoringStatus? {
+        val kotlinQueryParticipant = KotlinQueryParticipant()
+        val matches = arrayListOf<Match>()
+        val factory = JavaSearchScopeFactory.getInstance()
+        val querySpecification = ElementQuerySpecification(
+                element, 
+                IJavaSearchConstants.REFERENCES,
+                factory.createWorkspaceScope(false),
+                factory.getWorkspaceScopeDescription(false))
+        
+        kotlinQueryParticipant.search({ matches.add(it) }, querySpecification, NullProgressMonitor())
+        
+        matches
+            .map { createTextChange(it) }
+            .filterNotNull()
+            .forEach { changes.add(it) }
+        
+        return RefactoringStatus() // TODO: add corresponding refactoring status
     }
     
-    override fun createChange(pm: IProgressMonitor?): Change? {
-        println("cool")
-        return null
+    override fun getName() = "Kotlin Type Rename Participant"
+    
+    override fun createChange(pm: IProgressMonitor): Change {
+        return CompositeChange("Changes in Kotlin", changes.toTypedArray())
+    }
+    
+    private fun createTextChange(match: Match): Change? {
+        if (match !is KotlinElementMatch) return null
+        
+        val jetElement = match.jetElement
+        
+        val eclipseFile = KotlinPsiManager.getEclispeFile(jetElement.getContainingJetFile())
+        if (eclipseFile == null) return null
+        
+        val document = EditorUtil.getDocument(eclipseFile) // TODO: make workaround here later
+        
+        val change = TextFileChange("Rename Kotlin reference", eclipseFile)
+        change.setEdit(ReplaceEdit(jetElement.getTextDocumentOffset(document), jetElement.getTextLength(), newName))
+        
+        return change
     }
 }
