3cfaa8aec1604b6b72de3d6d8be495706c00b439
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-10-08 13:49:05 +0300

    Initial support of type rename refactoring (without jdt weaving)

---
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/refactorings/rename/KotlinTypeRenameParticipant.kt
@@ -0,0 +1,123 @@
+/*******************************************************************************
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+package org.jetbrains.kotlin.ui.refactorings.rename
+
+import org.eclipse.ltk.core.refactoring.participants.RenameParticipant
+import org.eclipse.core.runtime.IProgressMonitor
+import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext
+import org.eclipse.ltk.core.refactoring.RefactoringStatus
+import org.eclipse.ltk.core.refactoring.Change
+import kotlin.properties.Delegates
+import org.eclipse.jdt.core.IType
+import java.util.ArrayList
+import org.eclipse.ltk.core.refactoring.CompositeChange
+import org.jetbrains.kotlin.ui.search.KotlinQueryParticipant
+import org.eclipse.search.ui.text.Match
+import org.eclipse.jdt.ui.search.ElementQuerySpecification
+import org.eclipse.jdt.core.search.IJavaSearchConstants
+import org.eclipse.jdt.internal.ui.search.JavaSearchScopeFactory
+import org.eclipse.core.runtime.NullProgressMonitor
+import org.jetbrains.kotlin.ui.search.KotlinElementMatch
+import org.jetbrains.kotlin.core.builder.KotlinPsiManager
+import org.jetbrains.kotlin.eclipse.ui.utils.EditorUtil
+import org.eclipse.ltk.core.refactoring.TextFileChange
+import org.jetbrains.kotlin.eclipse.ui.utils.getTextDocumentOffset
+import org.eclipse.text.edits.ReplaceEdit
+import org.eclipse.ltk.core.refactoring.participants.RefactoringProcessor
+import org.eclipse.ltk.core.refactoring.participants.RefactoringArguments
+import org.eclipse.ltk.core.refactoring.participants.RenameArguments
+import org.eclipse.text.edits.TextEdit
+import org.eclipse.core.resources.IFile
+import org.eclipse.jdt.internal.corext.refactoring.changes.TextChangeCompatibility
+import org.jetbrains.kotlin.psi.JetNamedDeclaration
+
+public class KotlinTypeRenameParticipant : RenameParticipant() {
+    lateinit var element: IType
+    lateinit var newName: String
+    
+    val changes = arrayListOf<Change>()
+        
+    override fun initialize(element: Any): Boolean {
+        this.element = if (element is KotlinLightType) element.originElement else element as IType
+        changes.clear()
+        return true
+    }
+    
+    override fun initialize(processor: RefactoringProcessor, element: Any?, arguments: RefactoringArguments): Boolean {
+        if (arguments is RenameArguments) {
+            newName = arguments.getNewName()
+        }
+        
+        return super.initialize(processor, element, arguments)
+    }
+    
+    override fun checkConditions(pm: IProgressMonitor, context: CheckConditionsContext): RefactoringStatus? {
+        val kotlinQueryParticipant = KotlinQueryParticipant()
+        val matches = arrayListOf<Match>()
+        val factory = JavaSearchScopeFactory.getInstance()
+        val querySpecification = ElementQuerySpecification(
+                element, 
+                IJavaSearchConstants.ALL_OCCURRENCES,
+                factory.createWorkspaceScope(false),
+                factory.getWorkspaceScopeDescription(false))
+        
+        kotlinQueryParticipant.search({ matches.add(it) }, querySpecification, NullProgressMonitor())
+        
+        val groupedEdits = matches
+            .map { createTextChange(it) }
+            .filterNotNull()
+            .groupBy { it.file }
+        
+        for ((file, edits) in groupedEdits) {
+            val fileChange = TextFileChange("Kotlin change", file)
+            edits.forEach { TextChangeCompatibility.addTextEdit(fileChange, "Kotlin change", it.edit) }
+            
+            changes.add(fileChange)
+        }
+        
+        return RefactoringStatus() // TODO: add corresponding refactoring status
+    }
+    
+    override fun getName() = "Kotlin Type Rename Participant"
+    
+    override fun createChange(pm: IProgressMonitor): Change {
+        return CompositeChange("Changes in Kotlin", changes.toTypedArray())
+    }
+    
+    private fun createTextChange(match: Match): FileEdit? {
+        if (match !is KotlinElementMatch) return null
+        
+        val jetElement = match.jetElement
+        
+        val eclipseFile = KotlinPsiManager.getEclispeFile(jetElement.getContainingJetFile())
+        if (eclipseFile == null) return null
+        
+        val document = EditorUtil.getDocument(eclipseFile) // TODO: make workaround here later
+        
+        val textLength = if (jetElement is JetNamedDeclaration) {
+            jetElement.getNameIdentifier()!!.getTextLength()
+        } else {
+            jetElement.getTextLength()
+        }
+        
+        return FileEdit(
+                eclipseFile, 
+                ReplaceEdit(jetElement.getTextDocumentOffset(document), textLength, newName))
+    }
+}
+
+data class FileEdit(val file: IFile, val edit: TextEdit)
---
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/refactorings/rename/lightEclipseElements.kt
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+
+package org.jetbrains.kotlin.ui.refactorings.rename
+
+import org.eclipse.jdt.core.IType
+import org.jetbrains.kotlin.psi.JetElement
+import org.jetbrains.kotlin.ui.editors.KotlinFileEditor
+import org.eclipse.jdt.core.ISourceRange
+import org.eclipse.jdt.core.ICompilationUnit
+import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility
+import org.eclipse.jdt.internal.core.CompilationUnit
+import org.eclipse.jdt.core.IMethod
+import org.eclipse.jdt.core.IJavaElement
+import org.eclipse.core.resources.IFile
+import org.eclipse.jdt.internal.compiler.env.ICompilationUnit as ContentProviderCompilationUnit
+import org.eclipse.jdt.core.compiler.CharOperation
+import org.eclipse.core.resources.IResource
+import org.jetbrains.kotlin.eclipse.ui.utils.getTextDocumentOffset
+
+class KotlinLightType(val originElement: IType, jetElement: JetElement, val editor: KotlinFileEditor) : IType by originElement {
+    private val nameRange by lazy {
+        object : ISourceRange {
+            override fun getLength(): Int {
+                return 0
+            }
+            
+            override fun getOffset(): Int = jetElement.getTextDocumentOffset(editor.document)
+        }
+    }
+    
+    override fun getCompilationUnit(): ICompilationUnit {
+        val compilationUnit = EditorUtility.getEditorInputJavaElement(editor, false) as CompilationUnit
+        return KotlinLightCompilationUnit(editor.getFile()!!, compilationUnit)
+    }
+    
+    override fun getMethods(): Array<IMethod> = emptyArray()
+
+    override fun getNameRange(): ISourceRange = nameRange
+
+    override fun getPrimaryElement(): IJavaElement? = this
+
+    override fun isBinary(): Boolean = false
+
+    override fun isReadOnly(): Boolean = false
+}
+
+class KotlinLightCompilationUnit(val file: IFile, compilationUnit: ICompilationUnit) : ICompilationUnit by compilationUnit,
+       ContentProviderCompilationUnit {
+    override fun getFileName(): CharArray? = CharOperation.NO_CHAR
+    
+    override fun getContents(): CharArray? = CharOperation.NO_CHAR
+    
+    override fun ignoreOptionalProblems(): Boolean = true
+    
+    override fun getMainTypeName(): CharArray? = CharOperation.NO_CHAR
+    
+    override fun getPackageName(): Array<out CharArray>? {
+        return null
+    }
+
+    override fun getResource(): IResource = file
+}
