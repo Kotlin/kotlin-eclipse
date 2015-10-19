3c893ed3db5ce31a294e7f51b853d2013ece2eba
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-10-19 15:41:58 +0300

    Temp: fix tests for find reference

--- kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/search/KotlinFindReferencesTestCase.kt
+++ kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/search/KotlinFindReferencesTestCase.kt
@@ -64,7 +64,7 @@ abstract class KotlinFindReferencesTestCase : KotlinProjectTestCase() {
     protected fun doTest(filePath: String) {
         val (editor, sourceFiles, resultFile) = configureSourceFiles(filePath)
         
-        KotlinTestUtils.waitUntilIndexesReady()
+        KotlinTestUtils.joinBuildThread()
         
         val scope = SearchEngine.createWorkspaceScope()
         
