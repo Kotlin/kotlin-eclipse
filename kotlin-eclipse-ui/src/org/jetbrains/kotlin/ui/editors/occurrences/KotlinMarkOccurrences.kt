295fd2cb788556b360cee70c41bca1c20ad6c594
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-10-28 17:56:56 +0300

    Do not keep old occurrence annotations after selection changed

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/occurrences/KotlinMarkOccurrences.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/occurrences/KotlinMarkOccurrences.kt
@@ -48,41 +48,64 @@ import org.eclipse.core.runtime.Status
 import org.eclipse.core.resources.ResourcesPlugin
 import org.eclipse.core.resources.IFile
 import org.jetbrains.kotlin.ui.editors.withLock
+import org.eclipse.jface.text.source.AnnotationModel
+import org.eclipse.ui.progress.UIJob
 
-public class KotlinMarkOccurrences(val editor: KotlinFileEditor) : ISelectionListener {
-    private @Volatile var occurrenceAnnotations = setOf<Annotation>()
+public class KotlinMarkOccurrences : ISelectionListener {
+    companion object {
+        private val ANNOTATION_TYPE = "org.eclipse.jdt.ui.occurrences"
+    }
+    
+    private @Volatile var previousElement: JetElement? = null
     
     override fun selectionChanged(part: IWorkbenchPart, selection: ISelection) {
-        val job = object : Job("Mark occurrences") {
+        val job = object : Job("Update occurrence annotations") {
             override fun run(monitor: IProgressMonitor?): IStatus? {
                 if (part is KotlinFileEditor && selection is ITextSelection) {
                     val jetElement = EditorUtil.getJetElement(part, selection.getOffset())
-                    if (jetElement == null) return Status.CANCEL_STATUS
-                            
+                    if (jetElement == null || jetElement == previousElement) {
+                        previousElement = null
+                        return Status.CANCEL_STATUS
+                    } else {
+                        previousElement = jetElement
+                    }
+                    
                     val file = part.getFile()
                     if (file == null) return Status.CANCEL_STATUS
                     
-                    val occurrences = findOccurrences(jetElement, file)
-                    updateOccurrences(occurrences)
+                    val occurrences = findOccurrences(part, jetElement, file)
+                    updateOccurrences(part, occurrences)
                 }
                 
                 return Status.OK_STATUS
             }
         }
         
+        job.setPriority(Job.DECORATE)
         job.schedule()
     }
     
-    private @Synchronized fun updateOccurrences(occurrences: List<Position>) {
-        val annotationMap = occurrences.toMap { Annotation("org.eclipse.jdt.ui.occurrences", false, "description") }
+    private fun updateOccurrences(editor: KotlinFileEditor, occurrences: List<Position>) {
+        val annotationMap = occurrences.toMap { Annotation(ANNOTATION_TYPE, false, "description") }
         val annotationModel = editor.getDocumentProvider().getAnnotationModel(editor.getEditorInput())
+        val oldAnnotations = getOldOccurrenceAnnotations(annotationModel)
         annotationModel.withLock { 
-            (annotationModel as IAnnotationModelExtension).replaceAnnotations(occurrenceAnnotations.toTypedArray(), annotationMap)
-            occurrenceAnnotations = annotationMap.keySet()
+            (annotationModel as IAnnotationModelExtension).replaceAnnotations(oldAnnotations.toTypedArray(), annotationMap)
         }
     }
     
-    private @Synchronized fun findOccurrences(jetElement: JetElement, file: IFile): List<Position> {
+    private fun getOldOccurrenceAnnotations(model: IAnnotationModel): List<Annotation> {
+        val annotations = arrayListOf<Annotation>()
+        for (annotation in model.getAnnotationIterator()) {
+            if (annotation is Annotation && annotation.getType() == ANNOTATION_TYPE) {
+                annotations.add(annotation)
+            }
+        }
+        
+        return annotations
+    }
+    
+    private fun findOccurrences(editor: KotlinFileEditor, jetElement: JetElement, file: IFile): List<Position> {
         val sourceElements = jetElement.resolveToSourceDeclaration(editor.javaProject!!)
         if (sourceElements.isEmpty()) return emptyList()
         
