ea8c9f5a680fd30982862ebc94595e4a936002c1
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-10-30 15:03:05 +0300

    Fix tests: KotlinFunctionParameterInfoTest

--- kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/editors/completion/KotlinFunctionParameterInfoTest.java
+++ kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/editors/completion/KotlinFunctionParameterInfoTest.java
@@ -6,71 +6,71 @@ import org.junit.Test;
 public class KotlinFunctionParameterInfoTest extends KotlinFunctionParameterInfoTestCase {
     @Test
     public void testDeprecated() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/Deprecated.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/Deprecated.kt");
     }
 
     @Test
     public void testExtensionOnClassObject() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/ExtensionOnClassObject.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/ExtensionOnClassObject.kt");
     }
 
     @Test
     public void testInheritedFunctions() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/InheritedFunctions.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/InheritedFunctions.kt");
     }
 
     @Test
     public void testInheritedWithCurrentFunctions() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/InheritedWithCurrentFunctions.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/InheritedWithCurrentFunctions.kt");
     }
 
     @Test
     public void testNoAnnotations() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/NoAnnotations.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/NoAnnotations.kt");
     }
 
     @Test
     public void testNotAccessible() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/NotAccessible.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/NotAccessible.kt");
     }
 
     @Test
     public void testNotGreen() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/NotGreen.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/NotGreen.kt");
     }
 
     @Test
     public void testNullableTypeCall() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/NullableTypeCall.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/NullableTypeCall.kt");
     }
 
     @Test
     public void testPrintln() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/Println.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/Println.kt");
     }
 
     @Test
     public void testSimple() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/Simple.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/Simple.kt");
     }
 
     @Test
     public void testSimpleConstructor() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/SimpleConstructor.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/SimpleConstructor.kt");
     }
 
     @Test
     public void testSuperConstructorCall() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/SuperConstructorCall.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/SuperConstructorCall.kt");
     }
 
     @Test
     public void testTwoFunctions() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/TwoFunctions.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/TwoFunctions.kt");
     }
 
     @Test
     public void testTwoFunctionsGrey() throws Exception {
-        doTest("common_testData/ide/parameterInfo/functionParameterInfo/TwoFunctionsGrey.kt");
+        doTest("common_testData/ide/parameterInfo/functionCall/TwoFunctionsGrey.kt");
     }
 }
