460123f63d9a6a4ee63c1336121d4651c9bd4cd3
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-12-01 21:25:18 +0300

    temp

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/quickassist/KotlinTypeMismatchQuickFixGenerator.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/quickassist/KotlinTypeMismatchQuickFixGenerator.kt
@@ -1,4 +1,14 @@
 package org.jetbrains.kotlin.ui.editors.quickassist
 
-public class KotlinTypeMismatchQuickFixGenerator {
+import org.jetbrains.kotlin.ui.editors.KotlinFileEditor
+import com.intellij.psi.PsiElement
+
+public class KotlinTypeMismatchQuickFixGenerator : KotlinQuickAssistProposalsGenerator() {
+    override fun getProposals(kotlinFileEditor: KotlinFileEditor, psiElement: PsiElement): List<KotlinQuickAssistProposal> {
+        throw UnsupportedOperationException()
+    }
+    
+    override fun isApplicable(psiElement: PsiElement): Boolean {
+        throw UnsupportedOperationException()
+    }
 }
