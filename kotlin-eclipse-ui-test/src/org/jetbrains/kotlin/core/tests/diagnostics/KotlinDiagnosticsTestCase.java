423fc7347a598bb7e118b1dc780aa2c793631308
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2015-12-17 16:18:48 +0300

    Temporary join to build thread in tests for diagnostic (this fixes failing tests)

--- kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/core/tests/diagnostics/KotlinDiagnosticsTestCase.java
+++ kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/core/tests/diagnostics/KotlinDiagnosticsTestCase.java
@@ -56,6 +56,7 @@ import org.jetbrains.kotlin.resolve.calls.model.MutableResolvedCall;
 import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall;
 import org.jetbrains.kotlin.resolve.diagnostics.Diagnostics;
 import org.jetbrains.kotlin.testframework.editor.KotlinProjectTestCase;
+import org.jetbrains.kotlin.testframework.utils.KotlinTestUtils;
 import org.junit.Assert;
 import org.junit.Before;
 
@@ -187,7 +188,9 @@ public class KotlinDiagnosticsTestCase extends KotlinProjectTestCase {
 
             List<KtFile> jetFiles = getKtFiles(testFilesInModule, true);
             allKtFiles.addAll(jetFiles);
-
+            
+            KotlinTestUtils.joinBuildThread();
+            
             AnalysisResult analysisResult = EclipseAnalyzerFacadeForJVM.INSTANCE
                     .analyzeFilesWithJavaIntegration(getTestProject().getJavaProject(), getProject(), jetFiles)
                     .getAnalysisResult();
