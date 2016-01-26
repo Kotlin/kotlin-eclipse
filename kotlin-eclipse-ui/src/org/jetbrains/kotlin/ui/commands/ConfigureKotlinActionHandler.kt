d9ea851a8af03f264f854a1569eb4a2139f9b3a8
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-01-26 23:10:29 +0300

    Minor. Reformat and add license

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/commands/ConfigureKotlinActionHandler.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/commands/ConfigureKotlinActionHandler.kt
@@ -1,3 +1,19 @@
+/*******************************************************************************
+* Copyright 2000-2016 JetBrains s.r.o.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
 package org.jetbrains.kotlin.ui.commands
 
 import org.eclipse.core.commands.AbstractHandler
@@ -12,26 +28,26 @@ import org.jetbrains.kotlin.ui.launch.KotlinRuntimeConfigurationSuggestor
 import org.jetbrains.kotlin.core.model.KotlinNature
 
 public class ConfigureKotlinActionHandler : AbstractHandler() {
-	override fun execute(event: ExecutionEvent): Any? {
-		val selection = HandlerUtil.getActiveMenuSelection(event)
-		val project = getFirstOrNullJavaProject(selection as IStructuredSelection)!!.getProject()
-		
-		KotlinNature.addNature(project)
+    override fun execute(event: ExecutionEvent): Any? {
+        val selection = HandlerUtil.getActiveMenuSelection(event)
+        val project = getFirstOrNullJavaProject(selection as IStructuredSelection)!!.getProject()
+        
+        KotlinNature.addNature(project)
         KotlinRuntimeConfigurationSuggestor.suggestForProject(project);
-		
-		return null
-	}
-	
-	override fun setEnabled(evaluationContext: Any) {
-		val selection = HandlerUtil.getVariable(evaluationContext, ISources.ACTIVE_CURRENT_SELECTION_NAME)
-		if (selection is IStructuredSelection) {
-			val javaProject = getFirstOrNullJavaProject(selection)
-			if (javaProject != null) {
-				setBaseEnabled(isConfigurationMissing(javaProject.getProject()))
-				return
-			}
-		}
-		 
-		setBaseEnabled(false)
-	}
+        
+        return null
+    }
+    
+    override fun setEnabled(evaluationContext: Any) {
+        val selection = HandlerUtil.getVariable(evaluationContext, ISources.ACTIVE_CURRENT_SELECTION_NAME)
+        if (selection is IStructuredSelection) {
+            val javaProject = getFirstOrNullJavaProject(selection)
+            if (javaProject != null) {
+                setBaseEnabled(isConfigurationMissing(javaProject.getProject()))
+                return
+            }
+        }
+        
+        setBaseEnabled(false)
+    }
 }
