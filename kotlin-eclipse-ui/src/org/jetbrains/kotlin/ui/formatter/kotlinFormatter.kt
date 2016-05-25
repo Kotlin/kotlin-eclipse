3c0f0e6c01cf9711240186b9443d8025636e6d3a
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-05-25 05:07:57 +0300

    Enable new formatter

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/formatter/kotlinFormatter.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/formatter/kotlinFormatter.kt
@@ -1,16 +1,19 @@
 package org.jetbrains.kotlin.ui.formatter
 
-import com.intellij.formatting.Block
-import com.intellij.formatting.FormatTextRanges
-import com.intellij.formatting.FormatterImpl
+import com.intellij.formatting.*
 import com.intellij.openapi.editor.impl.DocumentImpl
+import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.TokenType
 import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.psi.impl.source.tree.TreeUtil
 import org.eclipse.jdt.core.IJavaProject
 import org.eclipse.jface.text.Document
 import org.eclipse.jface.text.IDocument
 import org.jetbrains.kotlin.core.model.KotlinEnvironment
 import org.jetbrains.kotlin.eclipse.ui.utils.IndenterUtil
+import org.jetbrains.kotlin.idea.formatter.KotlinSpacingBuilderUtil
+import org.jetbrains.kotlin.idea.formatter.createSpacingBuilder
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtPsiFactory
 
@@ -46,6 +49,16 @@ private class KotlinFormatter(source: String, psiFactory: KtPsiFactory, val init
     val sourceDocument = Document(source)
     
     fun formatCode(): String {
+        FormatterImpl()
+        val rootBlock = KotlinBlock(ktFile.getNode(), 
+                NULL_ALIGNMENT_STRATEGY, 
+                Indent.getNoneIndent(), 
+                null,
+                settings,
+                createSpacingBuilder(settings, KotlinSpacingBuilderUtilImpl))
+        
+        reformatAll(ktFile, rootBlock, settings, sourceDocument)
+        
         return sourceDocument.get()
     }
 }
@@ -61,3 +74,42 @@ private fun createKtFile(source: String, psiFactory: KtPsiFactory): KtFile {
 }
 
 private fun createWhitespaces(countSpaces: Int) = IndenterUtil.SPACE_STRING.repeat(countSpaces)
+
+object KotlinSpacingBuilderUtilImpl : KotlinSpacingBuilderUtil {
+    override fun createLineFeedDependentSpacing(minSpaces: Int,
+            maxSpaces: Int,
+            minimumLineFeeds: Int,
+            keepLineBreaks: Boolean,
+            keepBlankLines: Int,
+            dependency: TextRange,
+            rule: DependentSpacingRule): Spacing {
+        return object : DependantSpacingImpl(minSpaces, maxSpaces, dependency, keepLineBreaks, keepBlankLines, rule) {
+        }
+    }
+    
+    override fun getPreviousNonWhitespaceLeaf(node: com.intellij.lang.ASTNode?): com.intellij.lang.ASTNode? {
+        if (node == null) return null
+        val treePrev = node.treePrev
+        if (treePrev != null) {
+            val candidate = TreeUtil.getLastChild(treePrev)
+            if (candidate != null && !isWhitespaceOrEmpty(candidate)) {
+                return candidate
+            } else {
+                return getPreviousNonWhitespaceLeaf(candidate)
+            }
+        }
+        val treeParent = node.treeParent
+    
+        if (treeParent == null || treeParent.treeParent == null) {
+            return null
+        } else {
+            return getPreviousNonWhitespaceLeaf(treeParent)
+        }
+    }
+    
+    override fun isWhitespaceOrEmpty(node: com.intellij.lang.ASTNode?): kotlin.Boolean {
+        if (node == null) return false
+        val type = node.elementType
+        return type === TokenType.WHITE_SPACE || type !== TokenType.ERROR_ELEMENT && node.textLength == 0
+    }
+}
