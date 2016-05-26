eebd860d9eafc1bc79747741820bead5cc414905
Nikolay Krasko <goodwinnk@gmail.com>
2016-05-27 01:35:30 +0300

    Minor: reformat

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/filesystem/KotlinFileStore.kt
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/filesystem/KotlinFileStore.kt
@@ -48,90 +48,90 @@ import org.jetbrains.kotlin.core.model.KotlinAnalysisProjectCache
 import org.jetbrains.kotlin.core.resolve.KotlinAnalyzer
 
 public class KotlinFileStore(file: File) : LocalFile(file) {
-    override public fun openInputStream(options: Int, monitor: IProgressMonitor?): InputStream {
-        val javaProject = getJavaProject()
-        if (javaProject == null) {
-            throw CoreException(Status.CANCEL_STATUS)
-        }
-        
-        val jetFiles = KotlinLightClassManager.getInstance(javaProject).getSourceFiles(file)
-        if (jetFiles.isNotEmpty()) {
-            val analysisResult = KotlinAnalysisProjectCache.getAnalysisResultIfCached(javaProject) ?: 
-                    KotlinAnalyzer.analyzeFiles(javaProject, jetFiles).analysisResult
-            
-            val requestedClassName = Path(file.getAbsolutePath()).lastSegment()
-            val state = KotlinLightClassGeneration.buildLightClasses(analysisResult, javaProject, jetFiles, requestedClassName)
-            val generatedClass = state.factory.asList().find { 
-                val generatedClassName = Path(it.relativePath).lastSegment()
-                requestedClassName == generatedClassName
-            }
-            
-            if (generatedClass != null) return ByteArrayInputStream(generatedClass.asByteArray())
-        }
-        
-        throw CoreException(Status.CANCEL_STATUS)
-    }
-    
-    override public fun fetchInfo(options: Int, monitor: IProgressMonitor?): IFileInfo {
-        val info = super.fetchInfo(options, monitor) as FileInfo
-        if (Util.isClassFileName(getName())) {
-            val workspaceFile = findFileInWorkspace()
-            if (workspaceFile != null) {
-                info.setExists(workspaceFile.exists())
-            }
-        } else {
-            val workspaceContainer = findFolderInWorkspace()
-            if (workspaceContainer != null) {
-                info.setExists(workspaceContainer.exists())
-                info.setDirectory(true)
-            }
-        }
-        
-        return info
-    }
-    
-    override public fun childNames(options: Int, monitor: IProgressMonitor?): Array<String> {
-        val folder = findFolderInWorkspace()
-        if (folder != null && folder.exists()) {
-            return folder.members()
-                    .map { it.getName() }
-                    .toTypedArray()
-        }
-        
-        return emptyArray()
-    }
-    
-    override public fun mkdir(options: Int, monitor: IProgressMonitor?): IFileStore = this
-    
-    override public fun openOutputStream(options: Int, monitor: IProgressMonitor?): OutputStream = ByteArrayOutputStream()
-    
-    override public fun getChild(name: String): IFileStore = KotlinFileStore(File(file, name))
-    
-    override public fun getChild(path: IPath): IFileStore = KotlinFileStore(File(file, path.toOSString()))
-    
-    override public fun getFileStore(path: IPath): IFileStore = KotlinFileStore(Path(file.getPath()).append(path).toFile())
-    
-    override public fun getParent(): IFileStore? = file.getParentFile()?.let { KotlinFileStore(it) }
-    
-    private fun findFileInWorkspace(): IFile? {
-        val files = ResourcesPlugin.getWorkspace().getRoot().findFilesForLocationURI(file.toURI())
-        return if (files != null && files.isNotEmpty()) {
-            assert(files.size == 1, { "By ${file.toURI()} found more than one file" })
-            files[0]
-        } else {
-            null
-        }
-    }
-    
-    private fun findFolderInWorkspace(): IContainer? {
-        val containers = ResourcesPlugin.getWorkspace().getRoot().findContainersForLocationURI(file.toURI())
-        return if (containers != null && containers.isNotEmpty()) {
-            assert(containers.size == 1, { "By ${file.toURI()} found more than one file" })
-            containers[0]
-        } else {
-            null
-        }
-    }
-    
-    private fun getJavaProject(): IJavaProject? = findFileInWorkspace()?.let { JavaCore.create(it.getProject()) }
+	override public fun openInputStream(options: Int, monitor: IProgressMonitor?): InputStream {
+		val javaProject = getJavaProject()
+		if (javaProject == null) {
+			throw CoreException(Status.CANCEL_STATUS)
+		}
+
+		val jetFiles = KotlinLightClassManager.getInstance(javaProject).getSourceFiles(file)
+		if (jetFiles.isNotEmpty()) {
+			val analysisResult = KotlinAnalysisProjectCache.getAnalysisResultIfCached(javaProject) ?:
+			KotlinAnalyzer.analyzeFiles(javaProject, jetFiles).analysisResult
+
+			val requestedClassName = Path(file.getAbsolutePath()).lastSegment()
+			val state = KotlinLightClassGeneration.buildLightClasses(analysisResult, javaProject, jetFiles, requestedClassName)
+			val generatedClass = state.factory.asList().find {
+				val generatedClassName = Path(it.relativePath).lastSegment()
+				requestedClassName == generatedClassName
+			}
+
+			if (generatedClass != null) return ByteArrayInputStream(generatedClass.asByteArray())
+		}
+
+		throw CoreException(Status.CANCEL_STATUS)
+	}
+
+	override public fun fetchInfo(options: Int, monitor: IProgressMonitor?): IFileInfo {
+		val info = super.fetchInfo(options, monitor) as FileInfo
+		if (Util.isClassFileName(getName())) {
+			val workspaceFile = findFileInWorkspace()
+			if (workspaceFile != null) {
+				info.setExists(workspaceFile.exists())
+			}
+		} else {
+			val workspaceContainer = findFolderInWorkspace()
+			if (workspaceContainer != null) {
+				info.setExists(workspaceContainer.exists())
+				info.setDirectory(true)
+			}
+		}
+
+		return info
+	}
+
+	override public fun childNames(options: Int, monitor: IProgressMonitor?): Array<String> {
+		val folder = findFolderInWorkspace()
+		if (folder != null && folder.exists()) {
+			return folder.members()
+			.map { it.getName() }
+			.toTypedArray()
+		}
+
+		return emptyArray()
+	}
+
+	override public fun mkdir(options: Int, monitor: IProgressMonitor?): IFileStore = this
+
+	override public fun openOutputStream(options: Int, monitor: IProgressMonitor?): OutputStream = ByteArrayOutputStream()
+
+	override public fun getChild(name: String): IFileStore = KotlinFileStore(File(file, name))
+
+	override public fun getChild(path: IPath): IFileStore = KotlinFileStore(File(file, path.toOSString()))
+
+	override public fun getFileStore(path: IPath): IFileStore = KotlinFileStore(Path(file.getPath()).append(path).toFile())
+
+	override public fun getParent(): IFileStore? = file.getParentFile()?.let { KotlinFileStore(it) }
+
+	private fun findFileInWorkspace(): IFile? {
+		val files = ResourcesPlugin.getWorkspace().getRoot().findFilesForLocationURI(file.toURI())
+		return if (files != null && files.isNotEmpty()) {
+			assert(files.size == 1, { "By ${file.toURI()} found more than one file" })
+			files[0]
+		} else {
+			null
+		}
+	}
+
+	private fun findFolderInWorkspace(): IContainer? {
+		val containers = ResourcesPlugin.getWorkspace().getRoot().findContainersForLocationURI(file.toURI())
+		return if (containers != null && containers.isNotEmpty()) {
+			assert(containers.size == 1, { "By ${file.toURI()} found more than one file" })
+			containers[0]
+		} else {
+			null
+		}
+	}
+
+	private fun getJavaProject(): IJavaProject? = findFileInWorkspace()?.let { JavaCore.create(it.getProject()) }
 }
