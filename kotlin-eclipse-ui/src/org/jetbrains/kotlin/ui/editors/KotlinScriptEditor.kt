b9f4f8878eb04c460eee4de2976f88fb31744585
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-06-13 21:13:48 +0300

    Register basic editor services for scripts
     - Outline page, highlighting, formatter

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/KotlinScriptEditor.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/KotlinScriptEditor.kt
@@ -2,18 +2,45 @@ package org.jetbrains.kotlin.ui.editors
 
 import com.intellij.openapi.util.text.StringUtil
 import org.eclipse.core.resources.IFile
+import org.eclipse.jdt.core.IJavaProject
 import org.eclipse.jdt.core.JavaCore
 import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor
 import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor
+import org.eclipse.jdt.internal.ui.text.JavaColorManager
+import org.eclipse.jdt.ui.text.IColorManager
 import org.eclipse.jface.text.IDocument
+import org.eclipse.jface.text.ITextViewerExtension
+import org.eclipse.swt.widgets.Composite
 import org.jetbrains.kotlin.core.model.KotlinEnvironment
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtPsiFactory
-import org.eclipse.jdt.core.IJavaProject
+import org.jetbrains.kotlin.ui.editors.annotations.KotlinLineAnnotationsReconciler
+import org.jetbrains.kotlin.ui.editors.outline.KotlinOutlinePage
 
 class KotlinScriptEditor : CompilationUnitEditor(), KotlinEditor {
     
     private val kotlinReconcilingStrategy = KotlinReconcilingStrategy(this)
+    
+    private val colorManager: IColorManager = JavaColorManager()
+    
+    private val kotlinOutlinePage = KotlinOutlinePage(this)
+    
+    private val bracketInserter: KotlinBracketInserter = KotlinBracketInserter()
+    
+    override public fun createPartControl(parent: Composite) {
+        setSourceViewerConfiguration(FileEditorConfiguration(colorManager, this, getPreferenceStore(), kotlinReconcilingStrategy))
+        kotlinReconcilingStrategy.addListener(KotlinLineAnnotationsReconciler)
+        kotlinReconcilingStrategy.addListener(kotlinOutlinePage)
+        
+        super<CompilationUnitEditor>.createPartControl(parent)
+        
+        val sourceViewer = getSourceViewer()
+        if (sourceViewer is ITextViewerExtension) {
+            bracketInserter.setSourceViewer(sourceViewer)
+            bracketInserter.addBrackets('{', '}')
+            sourceViewer.prependVerifyKeyListener(bracketInserter)
+        }
+    }
     
     override val javaEditor: JavaEditor
         get() = this
