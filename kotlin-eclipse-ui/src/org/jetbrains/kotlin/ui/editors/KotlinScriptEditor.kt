c147d2234d4aee62a6cb0cedf5b18b84e1b528e7
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-06-13 20:19:55 +0300

    Register Kotlin editor for script files

---
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/KotlinScriptEditor.kt
@@ -0,0 +1,37 @@
+package org.jetbrains.kotlin.ui.editors
+
+import com.intellij.openapi.util.text.StringUtil
+import org.eclipse.core.resources.IFile
+import org.eclipse.jdt.core.JavaCore
+import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor
+import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor
+import org.eclipse.jface.text.IDocument
+import org.jetbrains.kotlin.core.model.KotlinEnvironment
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtPsiFactory
+import org.eclipse.jdt.core.IJavaProject
+
+class KotlinScriptEditor : CompilationUnitEditor(), KotlinEditor {
+    override val javaEditor: JavaEditor
+        get() = this
+
+    override val parsedFile: KtFile?
+        get() {
+            if (javaProject == null) return null
+            
+            val environment = KotlinEnvironment.getEnvironment(javaProject!!)
+            val ideaProject = environment.getProject()
+            val jetFile = KtPsiFactory(ideaProject).createFile(StringUtil.convertLineSeparators(document.get(), "\n"))
+            
+            return jetFile
+        }
+
+    override val javaProject: IJavaProject? by lazy {
+        getFile()?.let { JavaCore.create(it.getProject()) }
+    }
+
+    override val document: IDocument
+        get() = getDocumentProvider().getDocument(getEditorInput())
+    
+    fun getFile(): IFile? = getEditorInput().getAdapter(IFile::class.java)
+}
