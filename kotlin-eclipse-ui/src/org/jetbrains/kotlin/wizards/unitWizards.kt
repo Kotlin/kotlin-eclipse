3da2d09d1483010fba951c34d13b430b6498bbee
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-06-14 17:57:32 +0300

    Add wizard for script file creation

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/wizards/unitWizards.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/wizards/unitWizards.kt
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,55 @@
  *******************************************************************************/
 package org.jetbrains.kotlin.wizards
 
+import org.eclipse.core.resources.IFile
+import org.eclipse.core.runtime.IPath
+import org.eclipse.jface.viewers.IStructuredSelection
+import org.eclipse.ui.IWorkbench
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage
+import org.eclipse.ui.ide.IDE
+import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard
+import org.jetbrains.kotlin.parsing.KotlinParserDefinition
+
 class NewClassWizard : NewUnitWizard(WizardType.CLASS)
 class NewEnumWizard : NewUnitWizard(WizardType.ENUM)
 class NewObjectWizard : NewUnitWizard(WizardType.OBJECT)
 class NewInterfaceWizard : NewUnitWizard(WizardType.INTERFACE)
+
+class NewScriptWizard : BasicNewResourceWizard() {
+    companion object {
+        private const val pageName = "New Kotlin Script"
+    }
+    
+    private lateinit var mainPage: WizardNewFileCreationPage
+    
+    override fun addPages() {
+        super.addPages()
+        
+        mainPage = WizardNewFileCreationPage(pageName, getSelection()).apply {
+            setFileExtension(KotlinParserDefinition.STD_SCRIPT_SUFFIX)
+            setTitle("Kotlin Script")
+            setDescription("Create a new Kotlin script")
+        }
+        
+        addPage(mainPage)
+    }
+    
+    override fun init(workbench: IWorkbench, currentSelection: IStructuredSelection) {
+        super.init(workbench, currentSelection)
+        setWindowTitle(pageName)
+    }
+    
+    override fun performFinish(): Boolean {
+        val file = mainPage.createNewFile() ?: return false
+        
+        selectAndReveal(file)
+        workbench.activeWorkbenchWindow?.let {
+            val page = it.activePage
+            if (page != null) {
+                IDE.openEditor(page, file, true)
+            }
+        }
+        
+        return true
+    }
+}
