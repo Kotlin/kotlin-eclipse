7ab87dafe49eea9cb30321d78a7875fa785d7db2
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-07-18 18:56:25 +0300

    Minor, prettify and format

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/sources/LibrarySourcesIndex.kt
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/sources/LibrarySourcesIndex.kt
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
 package org.jetbrains.kotlin.core.resolve.sources
 
 import java.util.HashMap
@@ -15,37 +31,37 @@ import kotlin.io.use
 import org.eclipse.core.resources.ResourcesPlugin
 import org.jetbrains.kotlin.core.utils.ProjectUtils
 
-public class LibrarySourcesIndex(private val root: IPackageFragmentRoot) {
-    private val index: HashMap<String, ArrayList<SourceFile>>
+public class LibrarySourcesIndex(root: IPackageFragmentRoot) {
+    private val index = hashMapOf<String, ArrayList<SourceFile>>()
     private val PACKAGE_LINE_PREFIX = "package "
-    
+
+    private val sourcePath = ProjectUtils.convertToGlobalPath(root.resolvedClasspathEntry?.sourceAttachmentPath)
+
     init {
-        val sourcePath = ProjectUtils.convertToGlobalPath(
-            root.getResolvedClasspathEntry()?.getSourceAttachmentPath())
-        index = HashMap<String, ArrayList<SourceFile>>()
         if (sourcePath != null) {
             val sourceArchive = ZipFile(sourcePath.toOSString())
-            val names = sourceArchive.entries()
-                .toList()
-                .map { it.getName() }
-                .filter { KotlinSourceIndex.isKotlinSource(it) }
-            names.forEach {
-                val shortName = Path(it).lastSegment()
-                val listForFile = index.getOrPut(shortName) {ArrayList()}
-                listForFile.add(SourceFile(it))
-            }
+            
+            sourceArchive.entries().toList()
+                    .map { it.getName() }
+                    .filter { KotlinSourceIndex.isKotlinSource(it) }
+                    .forEach {
+                        val shortName = Path(it).lastSegment()
+                        index.getOrPut(shortName) { ArrayList() }.add(SourceFile(it))
+                    }
         }
     }
-    
+
     public fun resolve(shortName: String, packageFragment: IPackageFragment): String? {
-        val sourcesList = index.get(shortName) ?: return null
+        if (sourcePath == null) return null
+        
+        val sourcesList = index[shortName] ?: return null
         if (sourcesList.size == 1) {
             return sourcesList.first().path
         }
+        
         val packageName = packageFragment.getElementName()
-        val sourcePath = ProjectUtils.convertToGlobalPath(
-            root.getResolvedClasspathEntry()?.getSourceAttachmentPath())!!
         val sourceArchive = ZipFile(sourcePath.toOSString())
+        
         for (it in sourcesList) {
             if (it.effectivePackage == null) {
                 it.effectivePackage = getPackageName(sourceArchive, sourceArchive.getEntry(it.path))
@@ -53,18 +69,21 @@ public class LibrarySourcesIndex(private val root: IPackageFragmentRoot) {
             if (packageName == it.effectivePackage) {
                 return it.path
             }
-         }
+        }
+        
         return null
     }
-    
+
     private fun getPackageName(zipFile: ZipFile, entry: ZipEntry): String? {
         val istream = zipFile.getInputStream(entry)
         val reader = BufferedReader(InputStreamReader(istream))
         return reader.use {
             it.lineSequence()
-                .firstOrNull { it.startsWith(PACKAGE_LINE_PREFIX) }
-                ?.removePrefix(PACKAGE_LINE_PREFIX)
-                ?.trim()
+                    .firstOrNull { it.startsWith(PACKAGE_LINE_PREFIX) }
+                    ?.removePrefix(PACKAGE_LINE_PREFIX)
+                    ?.trim()
         }
     }
 }
+
+private class SourceFile(val path: String, var effectivePackage: String? = null)
