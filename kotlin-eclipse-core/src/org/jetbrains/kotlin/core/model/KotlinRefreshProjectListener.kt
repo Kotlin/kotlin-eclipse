1767c55bb15afeae55b4c77b409ff2455c30e589
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-10-12 17:31:50 +0300

    Actually update light classes on refresh

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/model/KotlinRefreshProjectListener.kt
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/model/KotlinRefreshProjectListener.kt
@@ -1,24 +1,35 @@
 package org.jetbrains.kotlin.core.model
 
-import org.eclipse.core.resources.IResourceChangeListener
-import org.eclipse.core.resources.IResourceChangeEvent
 import org.eclipse.core.resources.IProject
-import org.eclipse.jdt.core.JavaCore
+import org.eclipse.core.resources.IResource
+import org.eclipse.core.resources.IResourceChangeEvent
+import org.eclipse.core.resources.IResourceChangeListener
 import org.jetbrains.kotlin.core.asJava.KotlinLightClassGeneration
 
 public object KotlinRefreshProjectListener : IResourceChangeListener {
     override fun resourceChanged(event: IResourceChangeEvent) {
         if (event.getType() == IResourceChangeEvent.PRE_REFRESH) {
-            event.getDelta()?.accept { delta ->
-                val resource = delta.getResource()
-                if (resource != null && resource is IProject) {
-                    KotlinLightClassGeneration.updateLightClasses(resource, emptySet())
-                    
+            val delta = event.getDelta()
+            if (delta == null) {
+                tryUpdateLightClassesFor(event.resource)
+                return
+            }
+            
+            delta.accept { visitorDelta ->
+                val resource = visitorDelta.getResource()
+                if (resource is IProject) {
+                    tryUpdateLightClassesFor(resource)
                     return@accept false
                 }
                 
                 true
             }
+        }
+    }
+    
+    private fun tryUpdateLightClassesFor(resource: IResource?) {
+        if (resource is IProject && KotlinNature.hasKotlinNature(resource)) {
+            KotlinLightClassGeneration.updateLightClasses(resource, emptySet())
         }
     }
 }
