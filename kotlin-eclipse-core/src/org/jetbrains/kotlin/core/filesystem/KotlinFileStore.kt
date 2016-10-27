b6b1fef0cc741289b038cc292ba314fe9cee16c8
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-10-27 15:25:11 +0300

    Minor: reformat and update license

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/filesystem/KotlinFileStore.kt
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/filesystem/KotlinFileStore.kt
@@ -1,19 +1,19 @@
 /*******************************************************************************
-* Copyright 2000-2015 JetBrains s.r.o.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-*******************************************************************************/
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
 package org.jetbrains.kotlin.core.filesystem
 
 import org.eclipse.core.filesystem.IFileInfo
@@ -43,83 +43,83 @@ import java.io.OutputStream
 import java.net.URI
 
 public class KotlinFileStore(file: File) : LocalFile(file) {
-	override public fun openInputStream(options: Int, monitor: IProgressMonitor?): InputStream {
-		val project = getProject()
-		if (project == null) {
-			throw CoreException(Status.CANCEL_STATUS)
-		}
+    override public fun openInputStream(options: Int, monitor: IProgressMonitor?): InputStream {
+        val project = getProject()
+        if (project == null) {
+            throw CoreException(Status.CANCEL_STATUS)
+        }
 
-		val jetFiles = KotlinLightClassManager.getInstance(project).getSourceFiles(file)
-		if (jetFiles.isNotEmpty()) {
-			val analysisResult = KotlinAnalysisProjectCache.getAnalysisResultIfCached(project) ?:
-			KotlinAnalyzer.analyzeFiles(jetFiles).analysisResult
+        val jetFiles = KotlinLightClassManager.getInstance(project).getSourceFiles(file)
+        if (jetFiles.isNotEmpty()) {
+            val analysisResult = KotlinAnalysisProjectCache.getAnalysisResultIfCached(project) ?:
+                    KotlinAnalyzer.analyzeFiles(jetFiles).analysisResult
 
-			val requestedClassName = Path(file.getAbsolutePath()).lastSegment()
-			val state = KotlinLightClassGeneration.buildLightClasses(analysisResult, project, jetFiles, requestedClassName)
-			val generatedClass = state.factory.asList().find {
-				val generatedClassName = Path(it.relativePath).lastSegment()
-				requestedClassName == generatedClassName
-			}
+            val requestedClassName = Path(file.getAbsolutePath()).lastSegment()
+            val state = KotlinLightClassGeneration.buildLightClasses(analysisResult, project, jetFiles, requestedClassName)
+            val generatedClass = state.factory.asList().find {
+                val generatedClassName = Path(it.relativePath).lastSegment()
+                requestedClassName == generatedClassName
+            }
 
-			if (generatedClass != null) return ByteArrayInputStream(generatedClass.asByteArray())
-		}
+            if (generatedClass != null) return ByteArrayInputStream(generatedClass.asByteArray())
+        }
 
-		throw CoreException(Status.CANCEL_STATUS)
-	}
+        throw CoreException(Status.CANCEL_STATUS)
+    }
 
-	override public fun fetchInfo(options: Int, monitor: IProgressMonitor?): IFileInfo {
-		val info = super.fetchInfo(options, monitor) as FileInfo
-		if (Util.isClassFileName(getName())) {
-			val workspaceFile = findFileInWorkspace()
-			if (workspaceFile != null) {
-				info.setExists(workspaceFile.exists())
-			}
-		} else {
-			val workspaceContainer = findFolderInWorkspace()
-			if (workspaceContainer != null) {
-				info.setExists(workspaceContainer.exists())
-				info.setDirectory(true)
-			}
-		}
+    override public fun fetchInfo(options: Int, monitor: IProgressMonitor?): IFileInfo {
+        val info = super.fetchInfo(options, monitor) as FileInfo
+        if (Util.isClassFileName(getName())) {
+            val workspaceFile = findFileInWorkspace()
+            if (workspaceFile != null) {
+                info.setExists(workspaceFile.exists())
+            }
+        } else {
+            val workspaceContainer = findFolderInWorkspace()
+            if (workspaceContainer != null) {
+                info.setExists(workspaceContainer.exists())
+                info.setDirectory(true)
+            }
+        }
 
-		return info
-	}
+        return info
+    }
 
-	override public fun childNames(options: Int, monitor: IProgressMonitor?): Array<String> {
-		val folder = findFolderInWorkspace()
-		if (folder != null && folder.exists()) {
-			return folder.members()
-			.map { it.getName() }
-			.toTypedArray()
-		}
+    override public fun childNames(options: Int, monitor: IProgressMonitor?): Array<String> {
+        val folder = findFolderInWorkspace()
+        if (folder != null && folder.exists()) {
+            return folder.members()
+                    .map { it.getName() }
+                    .toTypedArray()
+        }
 
-		return emptyArray()
-	}
-    
+        return emptyArray()
+    }
+
     override fun getFileSystem(): IFileSystem = KotlinFileSystem.getInstance()
 
-	override public fun mkdir(options: Int, monitor: IProgressMonitor?): IFileStore = this
+    override public fun mkdir(options: Int, monitor: IProgressMonitor?): IFileStore = this
 
-	override public fun openOutputStream(options: Int, monitor: IProgressMonitor?): OutputStream = ByteArrayOutputStream()
+    override public fun openOutputStream(options: Int, monitor: IProgressMonitor?): OutputStream = ByteArrayOutputStream()
 
-	override public fun getChild(name: String): IFileStore = KotlinFileStore(File(file, name))
+    override public fun getChild(name: String): IFileStore = KotlinFileStore(File(file, name))
 
-	override public fun getChild(path: IPath): IFileStore = KotlinFileStore(File(file, path.toOSString()))
+    override public fun getChild(path: IPath): IFileStore = KotlinFileStore(File(file, path.toOSString()))
 
-	override public fun getFileStore(path: IPath): IFileStore = KotlinFileStore(Path(file.getPath()).append(path).toFile())
+    override public fun getFileStore(path: IPath): IFileStore = KotlinFileStore(Path(file.getPath()).append(path).toFile())
 
-	override public fun getParent(): IFileStore? = file.getParentFile()?.let { KotlinFileStore(it) }
-    
+    override public fun getParent(): IFileStore? = file.getParentFile()?.let { KotlinFileStore(it) }
+
     fun getProject(): IProject? = findFileInWorkspace()?.let { it.project }
 
-	private fun findFileInWorkspace(): IFile? {
+    private fun findFileInWorkspace(): IFile? {
         return findResourceInWorkspace { ResourcesPlugin.getWorkspace().root.findFilesForLocationURI(it) }
-	}
+    }
 
-	private fun findFolderInWorkspace(): IContainer? {
+    private fun findFolderInWorkspace(): IContainer? {
         return findResourceInWorkspace { ResourcesPlugin.getWorkspace().root.findContainersForLocationURI(it) }
-	}
-    
+    }
+
     private inline fun <reified T : IResource> findResourceInWorkspace(search: (URI) -> Array<T>?): T? {
         val absolutePath = file.toURI().path
         val pathRelatedToKtFileSystem = URI(KotlinFileSystem.SCHEME, null, absolutePath, null)
