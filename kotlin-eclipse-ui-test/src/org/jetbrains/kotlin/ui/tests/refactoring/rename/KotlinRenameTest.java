8aa896d6870c690a46b987b4d38e7d986f990800
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-01-31 02:23:02 +0300

    Revert "Temporary mute tests on rename"
    This reverts commit 21afd7a6328d580951563b51223e5247da201f40.

--- kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/refactoring/rename/KotlinRenameTest.java
+++ kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/refactoring/rename/KotlinRenameTest.java
@@ -4,94 +4,94 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 public class KotlinRenameTest extends KotlinRenameTestCase {
-//    @Test
-//    public void testSimple() {
-//        doTest("testData/refactoring/rename/simple/info.test");
-//    }
-//    
-//    @Test
-//    public void testAutomaticRenamer() {
-//        doTest("testData/refactoring/rename/automaticRenamer/simple.test");
-//    }
-//    
-//    @Test
-//    public void testRenameJavaClass() {
-//        doTest("testData/refactoring/rename/renameJavaClass/renameJavaClass.test");
-//    }
-//    
-//    @Test
-//    public void testRenameJavaClassSamePackage() {
-//        doTest("testData/refactoring/rename/renameJavaClassSamePackage/renameJavaClassSamePackage.test");
-//    }
-//    
-//    @Test
-//    public void testRenameJavaInterface() {
-//        doTest("testData/refactoring/rename/renameJavaInterface/renameJavaInterface.test");
-//    }
-//    
-//    @Ignore("Temporary disable as a bug")
-//    @Test
-//    public void testRenameJavaKotlinOverridenMethod() {
-//        doTest("testData/refactoring/rename/renameJavaMethod/kotlinOverridenMethod.test");
-//    }
-//
-//    @Test
-//    public void testRenameKotlinClass() {
-//        doTest("testData/refactoring/rename/renameKotlinClass/kotlinClass.test");
-//    }
-//    
-//    @Test
-//    public void testRenameKotlinMethod() {
-//        doTest("testData/refactoring/rename/renameKotlinMethod/renameKotlinMethod.test");
-//    }
-//    
-//    @Test
-//    public void testRenameKotlinTopLevelFun() {
-//        doTest("testData/refactoring/rename/renameKotlinTopLevelFun/renameKotlinTopLevelFun.test");
-//    }
-//    
-//    @Test
-//    public void testRenameJavaStaticMethod() {
-//        doTest("testData/refactoring/rename/renameJavaStaticMethod/renameJavaStaticMethod.test");
-//    }
-//    
-//    @Test
-//    public void testRenameKotlinClassByConstructorReference() {
-//        doTest("testData/refactoring/rename/renameKotlinClassByConstructorRef/renameKotlinClassByConstructorRef.test");
-//    }
-//    
-//    @Test
-//    public void testRenameKotlinClassFromJava() {
-//        doTest("testData/refactoring/rename/renameKotlinClassFromJava/renameKotlinClassFromJava.test");
-//    }
-//    
-//    @Test
-//    public void testRenameKotlinMethodFromJava() {
-//        doTest("testData/refactoring/rename/renameKotlinMethodFromJava/renameKotlinMethodFromJava.test");
-//    }
-//    
-//    @Test
-//    public void testRenameKotlinTopLevelFunFromJava() {
-//        doTest("testData/refactoring/rename/renameKotlinTopLevelFunFromJava/renameKotlinTopLevelFunFromJava.test");
-//    }
-//    
-//    @Test
-//    public void testRenameClassInScript() {
-//        doTest("testData/refactoring/rename/scripts/renameClassInScript/info.test");
-//    }
-//    
-//    @Test
-//    public void testRenameFunctionInScript() {
-//        doTest("testData/refactoring/rename/scripts/renameFunctionInScript/info.test");
-//    }
-//    
-//    @Test
-//    public void testRenamePropertyInScript() {
-//        doTest("testData/refactoring/rename/scripts/renamePropertyInScript/info.test");
-//    }
-//    
-//    @Test
-//    public void testRenameInScriptLocally() {
-//        doTest("testData/refactoring/rename/scripts/renameInScriptLocally/info.test");
-//    }
+    @Test
+    public void testSimple() {
+        doTest("testData/refactoring/rename/simple/info.test");
+    }
+    
+    @Test
+    public void testAutomaticRenamer() {
+        doTest("testData/refactoring/rename/automaticRenamer/simple.test");
+    }
+    
+    @Test
+    public void testRenameJavaClass() {
+        doTest("testData/refactoring/rename/renameJavaClass/renameJavaClass.test");
+    }
+    
+    @Test
+    public void testRenameJavaClassSamePackage() {
+        doTest("testData/refactoring/rename/renameJavaClassSamePackage/renameJavaClassSamePackage.test");
+    }
+    
+    @Test
+    public void testRenameJavaInterface() {
+        doTest("testData/refactoring/rename/renameJavaInterface/renameJavaInterface.test");
+    }
+    
+    @Ignore("Temporary disable as a bug")
+    @Test
+    public void testRenameJavaKotlinOverridenMethod() {
+        doTest("testData/refactoring/rename/renameJavaMethod/kotlinOverridenMethod.test");
+    }
+
+    @Test
+    public void testRenameKotlinClass() {
+        doTest("testData/refactoring/rename/renameKotlinClass/kotlinClass.test");
+    }
+    
+    @Test
+    public void testRenameKotlinMethod() {
+        doTest("testData/refactoring/rename/renameKotlinMethod/renameKotlinMethod.test");
+    }
+    
+    @Test
+    public void testRenameKotlinTopLevelFun() {
+        doTest("testData/refactoring/rename/renameKotlinTopLevelFun/renameKotlinTopLevelFun.test");
+    }
+    
+    @Test
+    public void testRenameJavaStaticMethod() {
+        doTest("testData/refactoring/rename/renameJavaStaticMethod/renameJavaStaticMethod.test");
+    }
+    
+    @Test
+    public void testRenameKotlinClassByConstructorReference() {
+        doTest("testData/refactoring/rename/renameKotlinClassByConstructorRef/renameKotlinClassByConstructorRef.test");
+    }
+    
+    @Test
+    public void testRenameKotlinClassFromJava() {
+        doTest("testData/refactoring/rename/renameKotlinClassFromJava/renameKotlinClassFromJava.test");
+    }
+    
+    @Test
+    public void testRenameKotlinMethodFromJava() {
+        doTest("testData/refactoring/rename/renameKotlinMethodFromJava/renameKotlinMethodFromJava.test");
+    }
+    
+    @Test
+    public void testRenameKotlinTopLevelFunFromJava() {
+        doTest("testData/refactoring/rename/renameKotlinTopLevelFunFromJava/renameKotlinTopLevelFunFromJava.test");
+    }
+    
+    @Test
+    public void testRenameClassInScript() {
+        doTest("testData/refactoring/rename/scripts/renameClassInScript/info.test");
+    }
+    
+    @Test
+    public void testRenameFunctionInScript() {
+        doTest("testData/refactoring/rename/scripts/renameFunctionInScript/info.test");
+    }
+    
+    @Test
+    public void testRenamePropertyInScript() {
+        doTest("testData/refactoring/rename/scripts/renamePropertyInScript/info.test");
+    }
+    
+    @Test
+    public void testRenameInScriptLocally() {
+        doTest("testData/refactoring/rename/scripts/renameInScriptLocally/info.test");
+    }
 }
