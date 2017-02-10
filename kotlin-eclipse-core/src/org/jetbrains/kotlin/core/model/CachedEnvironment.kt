e03a989a6d9b96edbaff2d5b95b56d3c09e579fe
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-02-10 14:51:32 +0300

    Minor: reformat CachedEnvironment

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/model/CachedEnvironment.kt
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/model/CachedEnvironment.kt
@@ -1,19 +1,19 @@
 /*******************************************************************************
-* Copyright 2000-2016 JetBrains s.r.o.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-*******************************************************************************/
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
 package org.jetbrains.kotlin.core.model
 
 import com.intellij.openapi.project.Project
@@ -23,42 +23,42 @@ import java.util.HashMap
 
 class CachedEnvironment<T, E : KotlinCommonEnvironment> {
     private val environmentLock = Any()
-    
+
     private val environmentCache: HashMap<T, E> = hashMapOf()
     private val ideaProjectToEclipseResource: HashMap<Project, T> = hashMapOf()
-    
+
     fun putEnvironment(resource: T, environment: E) {
         synchronized(environmentLock) {
             environmentCache.put(resource, environment)
             ideaProjectToEclipseResource.put(environment.project, resource)
         }
     }
-    
+
     fun getOrCreateEnvironment(resource: T, createEnvironment: (T) -> E): E {
         return synchronized(environmentLock) {
-                environmentCache.getOrPut(resource) {
+            environmentCache.getOrPut(resource) {
                 val newEnvironment = createEnvironment(resource)
                 ideaProjectToEclipseResource.put(newEnvironment.project, resource)
-                
+
                 newEnvironment
             }
         }
     }
-    
+
     fun removeEnvironment(resource: T) {
-        synchronized (environmentLock) {
+        synchronized(environmentLock) {
             if (environmentCache.containsKey(resource)) {
                 val environment = environmentCache[resource]!!
-                
+
                 ideaProjectToEclipseResource.remove(environment.project)
                 environmentCache.remove(resource)
-                
+
                 Disposer.dispose(environment.javaApplicationEnvironment.getParentDisposable())
                 ZipHandler.clearFileAccessorCache()
             }
         }
     }
-    
+
     fun getEclipseResource(ideaProject: Project): T? {
         return synchronized(environmentLock) {
             ideaProjectToEclipseResource.get(ideaProject)
