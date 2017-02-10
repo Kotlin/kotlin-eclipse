7f1840a027502cd367fcab8533cb1da86eff61d3
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-02-10 14:43:30 +0300

    Introduce updated API for ScriptTemplateProvider extension

--- kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/scripts/templates/TestKtScriptTemplateProviderEx.kt
+++ kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/scripts/templates/TestKtScriptTemplateProviderEx.kt
@@ -4,13 +4,12 @@ import org.jetbrains.kotlin.core.model.ScriptTemplateProviderEx
 import org.eclipse.core.resources.IFile
 
 class TestKtScriptTemplateProviderEx : ScriptTemplateProviderEx {
-    override val templateClassClasspath = listOf("bin/", "target/classes/")
     override val templateClassName = "org.jetbrains.kotlin.ui.tests.scripts.templates.TestScriptTemplateDefinitionEx"
-
-    override fun isApplicable(file: IFile): Boolean {
-        return file.name.contains("Ex")
-    }
     
+    override fun getTemplateClasspath(environment: Map<String, Any?>?): Iterable<String> {
+        return listOf("bin/", "target/classes/")
+    }
+
     override fun getEnvironment(file: IFile): Map<String, Any?>? {
         return mapOf(
                 "projectName" to file.project.name,
--- kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/scripts/templates/testCustomEPResolver.kt
+++ kotlin-eclipse-ui-test/src/org/jetbrains/kotlin/ui/tests/scripts/templates/testCustomEPResolver.kt
@@ -12,15 +12,20 @@ import org.junit.Assert
 import java.util.concurrent.Future
 
 class CustomEPResolverScriptTemplateProvider : ScriptTemplateProviderEx {
-    override val templateClassClasspath = listOf("bin/", "target/classes/")
     override val templateClassName = "org.jetbrains.kotlin.ui.tests.scripts.templates.CustomReolverScriptTemplateDefinition"
-
-    override fun isApplicable(file: IFile): Boolean {
-        return file.name.contains("customEPResolver")
-    }
     
-    override val resolver: ScriptDependenciesResolver get() = CustomScriptDependenciesResolver()
+    override fun getTemplateClasspath(environment: Map<String, Any?>?): Iterable<String> {
+        return listOf("bin/", "target/classes/")
+    }
+
+    override fun getEnvironment(file: IFile): Map<String, Any?>? = null
 }
+
+@ScriptTemplateDefinition(
+        resolver = CustomScriptDependenciesResolver::class,
+        scriptFilePattern = "customEPResolver.kts"
+)
+open class CustomReolverScriptTemplateDefinition
 
 class CustomScriptDependenciesResolver : ScriptDependenciesResolver {
     override fun resolve(
@@ -29,18 +34,7 @@ class CustomScriptDependenciesResolver : ScriptDependenciesResolver {
             report: (ScriptDependenciesResolver.ReportSeverity, String, ScriptContents.Position?) -> Unit,
             previousDependencies: KotlinScriptExternalDependencies?): Future<KotlinScriptExternalDependencies?> {
         return object : KotlinScriptExternalDependencies {
-            override val imports: Iterable<String> get() {
-                // Test workspace is available
-                Assert.assertTrue(ResourcesPlugin.getWorkspace().getRoot().getLocation() != null)
-                
-                return listOf("java.util.Date")
-            }
+            override val imports: Iterable<String> get() = listOf("java.util.Date")
         }.asFuture()
     }
 }
-
-@ScriptTemplateDefinition(
-        resolver = TestKotlinScriptResolver::class, // Default resolver that doesn't conform resolver in extenstion point
-        scriptFilePattern = "customEPResolver.kts"
-)
-open class CustomReolverScriptTemplateDefinition
