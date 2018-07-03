cb28d3715d7dd88728631dc14b01ccaaf386e6d0
Wieslaw Popielarski <wpopielarski@gmail.com>
2018-07-03 15:41:19 +0200

    Organize imports

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/lang/java/structure/EclipseJavaTypeAsAnnotationArgument.kt
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/resolve/lang/java/structure/EclipseJavaTypeAsAnnotationArgument.kt
@@ -1,12 +1,9 @@
 package org.jetbrains.kotlin.core.resolve.lang.java.structure
 
 import org.eclipse.jdt.core.dom.ITypeBinding
-import org.jetbrains.kotlin.load.java.structure.JavaEnumValueAnnotationArgument
-import org.jetbrains.kotlin.load.java.structure.JavaType
-import org.jetbrains.kotlin.name.ClassId
-import org.jetbrains.kotlin.name.FqName
-import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.load.java.structure.JavaClassObjectAnnotationArgument
+import org.jetbrains.kotlin.load.java.structure.JavaType
+import org.jetbrains.kotlin.name.Name
 
 public class EclipseJavaTypeAsAnnotationArgument(binding: ITypeBinding, override val name: Name?)
 	: EclipseJavaAnnotationArgument<ITypeBinding>(binding), JavaClassObjectAnnotationArgument {
