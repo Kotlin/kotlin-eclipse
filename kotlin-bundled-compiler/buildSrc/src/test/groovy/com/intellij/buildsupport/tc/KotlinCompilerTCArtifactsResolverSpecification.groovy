77c012337013f037b7b8025b2c5860ec99611501
Martin Skurla <crazyjavahacking@gmail.com>
2018-10-10 12:50:27 +0200

    Added support for recursive build search when 0 artifacts are published for a TC build, added integration tests

--- kotlin-bundled-compiler/buildSrc/src/test/groovy/com/intellij/buildsupport/tc/KotlinCompilerTCArtifactsResolverSpecification.groovy
+++ kotlin-bundled-compiler/buildSrc/src/test/groovy/com/intellij/buildsupport/tc/KotlinCompilerTCArtifactsResolverSpecification.groovy
@@ -2,8 +2,11 @@ package com.intellij.buildsupport.tc
 
 
 import com.intellij.buildsupport.resolve.tc.TCArtifactsResolver
+
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import java.text.SimpleDateFormat
 
 
 class KotlinCompilerTCArtifactsResolverSpecification extends Specification {
@@ -12,10 +15,11 @@ class KotlinCompilerTCArtifactsResolverSpecification extends Specification {
 
 
     @Unroll
-    def "should be able to resolve TC artifacts by buildId for version #releaseDescribeVersion"() {
+    def "should resolve TC artifacts by buildId for version #releaseDescribeVersion"() {
         setup:
             def tcArtifactResolver = tcArtifactResolverWithBuildId(tcBuildId,
                                                                    kotlinIdeaCompatibleVersionMinor)
+
             def temporaryFile = File.createTempFile("kotlin-compiler-$releaseDescribeVersion", '.tmp')
 
         expect:
@@ -31,10 +35,11 @@ class KotlinCompilerTCArtifactsResolverSpecification extends Specification {
     }
 
     @Unroll
-    def "should be able to resolve TC artifacts by latest build for version #releaseDescribeVersion"() {
+    def "should resolve TC artifacts for latest successful build for version #kotlinCompilerVersion"() {
         setup:
             def tcArtifactResolver = tcArtifactResolverWithLatestBuildAndKotlinCompilerVersion(kotlinCompilerVersion,
                                                                                                kotlinIdeaCompatibleVersionMinor)
+
             def temporaryFile = File.createTempFile("kotlin-compiler-latest-$kotlinCompilerVersion", '.tmp')
 
         expect:
@@ -43,11 +48,33 @@ class KotlinCompilerTCArtifactsResolverSpecification extends Specification {
             assert temporaryFile.exists()
 
         where:
-            releaseDescribeVersion | kotlinCompilerVersion | kotlinIdeaCompatibleVersionMinor
-            '1.2.70'               | '1.2.70'              | '2017.3'
-            '1.3-M1'               | '1.3-M1'              | '2017.3'
-            '1.3-M2'               | '1.3-M2'              | '2017.3'
-            '1.3.0'                | '1.3.0'               | '2017.3'
+            kotlinCompilerVersion | kotlinIdeaCompatibleVersionMinor
+            '1.2.70'              | '2017.3'
+            '1.3-M1'              | '2017.3'
+            '1.3-M2'              | '2017.3'
+            '1.3.0'               | '2017.3'
+    }
+
+    @Unroll
+    def "should resolve TC artifacts for latest successful build without TC artifacts for version #kotlinCompilerVersion"() {
+        setup:
+            def tcArtifactResolver = tcArtifactResolverWithLatestBuildAndKotlinCompilerVersion(kotlinCompilerVersion,
+                                                                                               kotlinIdeaCompatibleVersionMinor,
+                                                                                               untilDate)
+
+            def temporaryFile = File.createTempFile("kotlin-compiler-latest-$kotlinCompilerVersion", '.tmp')
+
+        expect:
+            tcArtifactResolver.downloadTo tcArtifactResolver.KOTLIN_PLUGIN_ZIP, temporaryFile
+
+            assert temporaryFile.exists()
+
+        where:
+            untilDate    | kotlinCompilerVersion | kotlinIdeaCompatibleVersionMinor
+            '2018-08-16' | '1.2.60'              | '2017.3' // 3  builds with 0 artifacts
+            '2018-09-10' | '1.2.70'              | '2017.3' // 4  builds with 0 artifacts
+            '2018-08-24' | '1.3-M2'              | '2017.3' // 3  builds with 0 artifacts
+            '2018-10-03' | '1.3.0'               | '2017.3' // 10 builds with 0 artifacts
     }
 
 
@@ -62,10 +89,23 @@ class KotlinCompilerTCArtifactsResolverSpecification extends Specification {
 
     private static TCArtifactsResolver tcArtifactResolverWithLatestBuildAndKotlinCompilerVersion(String kotlinCompilerVersion,
                                                                                                  String kotlinIdeaCompatibleVersionMinor) {
-        return new TCArtifactsResolver(TC_BASE_URL,
-                true,  // searching for last successful build
-                '', // buildId not used
-                kotlinCompilerVersion,
-                kotlinIdeaCompatibleVersionMinor)
+        return tcArtifactResolverWithLatestBuildAndKotlinCompilerVersion(kotlinCompilerVersion,
+                                                                         kotlinIdeaCompatibleVersionMinor,
+                                                                         null)
+    }
+
+    private static TCArtifactsResolver tcArtifactResolverWithLatestBuildAndKotlinCompilerVersion(String kotlinCompilerVersion,
+                                                                                                 String kotlinIdeaCompatibleVersionMinor,
+                                                                                                 String untilDate) {
+        TCArtifactsResolver tcArtifactsResolver =  new TCArtifactsResolver(TC_BASE_URL,
+                                                                           true,  // searching for last successful build
+                                                                           '', // buildId not used
+                                                                           kotlinCompilerVersion,
+                                                                           kotlinIdeaCompatibleVersionMinor)
+
+        if (untilDate != null)
+            tcArtifactsResolver.untilDate = new SimpleDateFormat("yyyy-MM-dd").parse(untilDate)
+
+        return tcArtifactsResolver
     }
 }
