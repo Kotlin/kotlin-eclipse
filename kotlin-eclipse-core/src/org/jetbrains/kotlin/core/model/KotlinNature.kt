136d81446ff9b0be5f47529659793a3faf55caf6
Paweł Marks <pmarks@virtuslab.com>
2019-01-03 16:14:44 +0100

    Adds project configurator for gradle

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/model/KotlinNature.kt
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/model/KotlinNature.kt
@@ -1,62 +1,72 @@
 /*******************************************************************************
-* Copyright 2000-2015 JetBrains s.r.o.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-*******************************************************************************/
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
 package org.jetbrains.kotlin.core.model
 
-import org.eclipse.core.resources.IProject
-import org.eclipse.core.resources.IProjectNature
-import org.eclipse.core.resources.ProjectScope
-import kotlin.properties.Delegates
-import org.jetbrains.kotlin.core.builder.KotlinPsiManager
-import java.util.LinkedList
-import org.eclipse.core.runtime.jobs.Job
+import org.eclipse.core.resources.*
 import org.eclipse.core.runtime.IProgressMonitor
 import org.eclipse.core.runtime.IStatus
-import org.eclipse.jdt.core.JavaCore
-import java.util.Collections
 import org.eclipse.core.runtime.Status
-import org.eclipse.core.resources.ResourcesPlugin
+import org.eclipse.core.runtime.jobs.Job
+import org.eclipse.jdt.core.JavaCore
+import org.jetbrains.kotlin.core.builder.KotlinPsiManager
 import org.jetbrains.kotlin.core.preferences.KotlinCodeStyleProperties
+import java.util.*
+import kotlin.properties.Delegates
 
-class KotlinNature: IProjectNature {
+class KotlinNature : IProjectNature {
     companion object {
         val KOTLIN_NATURE: String = "org.jetbrains.kotlin.core.kotlinNature"
-        @JvmField val KOTLIN_BUILDER: String = "org.jetbrains.kotlin.ui.kotlinBuilder"
-        
+        @JvmField
+        val KOTLIN_BUILDER: String = "org.jetbrains.kotlin.ui.kotlinBuilder"
+
         @JvmStatic
-        fun hasKotlinNature(project: IProject) : Boolean {
+        fun hasKotlinNature(project: IProject): Boolean {
             return project.hasNature(KOTLIN_NATURE)
         }
-        
+
         @JvmStatic
-        fun hasKotlinBuilder(project: IProject) : Boolean {
+        fun hasKotlinBuilder(project: IProject): Boolean {
             if (!project.isAccessible) return false
-            
+
             return project.description.buildSpec.any {
                 KOTLIN_BUILDER == it.builderName
             }
         }
-        
+
         @JvmStatic
-        fun addNature(project:IProject) {
-            if (!hasKotlinNature(project)) {
-                val description = project.description
-                description.natureIds += KOTLIN_NATURE
-                project.setDescription(description, null)
+        fun addNature(project: IProject) {
+            if (hasKotlinNature(project)) return
+
+            project.modifyDescription {
+                natureIds += KOTLIN_NATURE
             }
+        }
+
+        fun removeNature(project: IProject) {
+            if (!hasKotlinNature(project)) return
+
+            project.modifyDescription {
+                natureIds = natureIds.filterNot { it == KOTLIN_NATURE }.toTypedArray()
+            }
+        }
+
+        private fun IProject.modifyDescription(operation: IProjectDescription.() -> Unit) {
+            description.apply(operation)
+                .also { setDescription(it, null) }
         }
     }
 
@@ -79,13 +89,13 @@ class KotlinNature: IProjectNature {
     }
 
     override fun getProject(): IProject = eclipseProject
-    
+
     private fun addKotlinBuilder(project: IProject) {
         if (!hasKotlinBuilder(project)) {
             val description = project.description
-            
+
             val kotlinBuilderCommand = description.newCommand().apply { builderName = KOTLIN_BUILDER }
-            
+
             val newBuildCommands = description.buildSpec.toCollection(LinkedList())
             newBuildCommands.addFirst(kotlinBuilderCommand)
 
@@ -102,16 +112,16 @@ class KotlinNature: IProjectNature {
         }
 
     }
-    
+
     private fun removeKotlinBuilder(project: IProject) {
-		if (hasKotlinBuilder(project)) {
-			val description = project.description
-			val newBuildCommands = description.buildSpec.filter { it.builderName != KotlinNature.KOTLIN_BUILDER }
+        if (hasKotlinBuilder(project)) {
+            val description = project.description
+            val newBuildCommands = description.buildSpec.filter { it.builderName != KotlinNature.KOTLIN_BUILDER }
 
             description.buildSpec = newBuildCommands.toTypedArray()
-			project.setDescription(description, null)
-		}
-	}
+            project.setDescription(description, null)
+        }
+    }
 }
 
 // Place Kotlin Builder before Java Builder to avoid red code in Java about Kotlin references 
@@ -119,18 +129,18 @@ fun setKotlinBuilderBeforeJavaBuilder(project: IProject) {
     val job = object : Job("Swap Kotlin builder with Java Builder") {
         override fun run(monitor: IProgressMonitor?): IStatus? {
             val description = project.description
-                
+
             val builders = description.buildSpec.toCollection(LinkedList())
             val kotlinBuilderIndex = builders.indexOfFirst { it.builderName == KotlinNature.KOTLIN_BUILDER }
             val javaBuilderIndex = builders.indexOfFirst { it.builderName == JavaCore.BUILDER_ID }
-            
+
             if (kotlinBuilderIndex >= 0 && javaBuilderIndex >= 0 && javaBuilderIndex < kotlinBuilderIndex) {
                 Collections.swap(builders, kotlinBuilderIndex, javaBuilderIndex)
 
                 description.buildSpec = builders.toTypedArray()
                 project.setDescription(description, monitor)
             }
-            
+
             return Status.OK_STATUS
         }
     }
