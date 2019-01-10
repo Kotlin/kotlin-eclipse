c66c85ec9c7bf441b42f355f54155ee95021c18f
Paweł Marks <pmarks@virtuslab.com>
2019-01-10 12:40:30 +0100

    Makes changes more friendly for packaged distribution

--- kotlin-eclipse-gradle/src/org/jetbrains/kotlin/gradle/initialization/ModelInjector.kt
+++ kotlin-eclipse-gradle/src/org/jetbrains/kotlin/gradle/initialization/ModelInjector.kt
@@ -6,25 +6,26 @@ import org.jetbrains.kotlin.gradle.Activator
 import org.eclipse.core.runtime.FileLocator
 import java.io.File
 import java.lang.management.ManagementFactory
+import org.jetbrains.kotlin.core.log.KotlinLogger
 
 class ModelInjector : InvocationCustomizer {
     override fun getExtraArguments() = listOf("--init-script", initializerPath)
         .also { System.setProperty(MODEL_PATH_PROPERTY, modelLibPath) }
-    
+
     companion object {
         private const val MODEL_PATH_PROPERTY = "org.jetbrains.kotlin.eclipse.gradle.model.path"
 
         private val initializerPath by lazy {
             Platform.getBundle(Activator.PLUGIN_ID)
-                    .getEntry("scripts/init.gradle.kts")
-                    .let { FileLocator.resolve(it) }
-                    .path
+                .getEntry("scripts/init.gradle.kts")
+                .let(FileLocator::toFileURL)
+                .path
         }
 
         private val modelLibPath by lazy<String> {
             Platform.getBundle("org.jetbrains.kotlin.gradle.model")
                 .getEntry("lib/kotlin-eclipse-gradle-model.jar")
-                .let { FileLocator.resolve(it) }
+                .let(FileLocator::toFileURL)
                 .path
         }
     }
