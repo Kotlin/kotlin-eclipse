425a08ab111c58f4208648569af68b1468f78984
Błażej Kardyś <bkardys@virtuslab.com>
2019-04-23 17:48:56 +0200

    Adjusting code completion mechanism for better performance

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/FileEditorConfiguration.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/FileEditorConfiguration.kt
@@ -16,31 +16,28 @@
  *******************************************************************************/
 package org.jetbrains.kotlin.ui.editors
 
+import org.eclipse.jdt.ui.PreferenceConstants
 import org.eclipse.jdt.ui.text.IColorManager
 import org.eclipse.jface.preference.IPreferenceStore
-import org.eclipse.jface.text.quickassist.QuickAssistAssistant
+import org.eclipse.jface.preference.PreferenceConverter
+import org.eclipse.jface.text.IDocument
+import org.eclipse.jface.text.contentassist.ContentAssistant
+import org.eclipse.jface.text.contentassist.IContentAssistant
 import org.eclipse.jface.text.quickassist.IQuickAssistAssistant
-import org.eclipse.jface.text.source.ISourceViewer
+import org.eclipse.jface.text.quickassist.QuickAssistAssistant
 import org.eclipse.jface.text.reconciler.IReconciler
 import org.eclipse.jface.text.reconciler.MonoReconciler
-import org.eclipse.jface.text.IAutoEditStrategy
-import org.eclipse.jface.text.contentassist.IContentAssistant
-import org.eclipse.jface.text.contentassist.ContentAssistant
-import org.jetbrains.kotlin.ui.editors.codeassist.KotlinCompletionProcessor
-import org.eclipse.jface.text.IDocument
-import org.eclipse.jdt.ui.PreferenceConstants
-import org.eclipse.jface.preference.PreferenceConverter
+import org.eclipse.jface.text.source.ISourceViewer
 import org.eclipse.swt.graphics.Color
-import org.jetbrains.kotlin.ui.editors.annotations.KotlinLineAnnotationsReconciler
-import org.jetbrains.kotlin.ui.editors.highlighting.KotlinSemanticHighlighter
+import org.jetbrains.kotlin.ui.editors.codeassist.KotlinCompletionProcessor
 
-public class FileEditorConfiguration(colorManager: IColorManager,
+class FileEditorConfiguration(colorManager: IColorManager,
         private val fileEditor: KotlinEditor,
         preferenceStore: IPreferenceStore,
         private val reconcilingStrategy: KotlinReconcilingStrategy): Configuration(colorManager, fileEditor, preferenceStore) {
     override fun getQuickAssistAssistant(sourceViewer: ISourceViewer): IQuickAssistAssistant? {
         val quickAssist = QuickAssistAssistant()
-        quickAssist.setQuickAssistProcessor(KotlinCorrectionProcessor(fileEditor))
+        quickAssist.quickAssistProcessor = KotlinCorrectionProcessor(fileEditor)
         quickAssist.setInformationControlCreator(getInformationControlCreator(sourceViewer))
         return quickAssist
     }
@@ -52,38 +49,34 @@ public class FileEditorConfiguration(colorManager: IColorManager,
     override fun getAutoEditStrategies(sourceViewer: ISourceViewer, contentType: String) =
             arrayOf(KotlinAutoIndentStrategy(fileEditor))
 
-    override fun getContentAssistant(sourceViewer: ISourceViewer): IContentAssistant? {
-        val assistant = ContentAssistant()
-        val completionProcessor = KotlinCompletionProcessor(fileEditor, assistant)
-
-        assistant.setContentAssistProcessor(completionProcessor, IDocument.DEFAULT_CONTENT_TYPE)
-        assistant.addCompletionListener(completionProcessor)
+    override fun getContentAssistant(sourceViewer: ISourceViewer): IContentAssistant? = ContentAssistant().apply {
+        KotlinCompletionProcessor.createKotlinCompletionProcessors(fileEditor, this).forEach {
+            addContentAssistProcessor(it, IDocument.DEFAULT_CONTENT_TYPE)
+        }
         
         val autoActivation = fPreferenceStore.getBoolean(PreferenceConstants.CODEASSIST_AUTOACTIVATION)
-        assistant.enableAutoActivation(autoActivation)
+        enableAutoActivation(autoActivation)
         
         val delay = fPreferenceStore.getInt(PreferenceConstants.CODEASSIST_AUTOACTIVATION_DELAY)
-        assistant.setAutoActivationDelay(delay)
+        autoActivationDelay = delay
         
-        val foregroundColor = getColor(fPreferenceStore, PreferenceConstants.CODEASSIST_PARAMETERS_FOREGROUND, getColorManager())
-        assistant.setContextInformationPopupForeground(foregroundColor)
-        assistant.setContextSelectorForeground(foregroundColor)
+        val foregroundColor = getColor(fPreferenceStore, PreferenceConstants.CODEASSIST_PARAMETERS_FOREGROUND, colorManager)
+        setContextInformationPopupForeground(foregroundColor)
+        setContextSelectorForeground(foregroundColor)
         
-        val backgroundColor = getColor(fPreferenceStore, PreferenceConstants.CODEASSIST_PARAMETERS_BACKGROUND, getColorManager())
-        assistant.setContextInformationPopupBackground(backgroundColor)
-        assistant.setContextSelectorBackground(backgroundColor)
+        val backgroundColor = getColor(fPreferenceStore, PreferenceConstants.CODEASSIST_PARAMETERS_BACKGROUND, colorManager)
+        setContextInformationPopupBackground(backgroundColor)
+        setContextSelectorBackground(backgroundColor)
         
         val autoInsert = fPreferenceStore.getBoolean(PreferenceConstants.CODEASSIST_AUTOINSERT)
-        assistant.enableAutoInsert(autoInsert)
+        enableAutoInsert(autoInsert)
         
-        assistant.setProposalPopupOrientation(IContentAssistant.PROPOSAL_OVERLAY)
-        assistant.setContextInformationPopupOrientation(IContentAssistant.CONTEXT_INFO_ABOVE)
+        setProposalPopupOrientation(IContentAssistant.PROPOSAL_OVERLAY)
+        setContextInformationPopupOrientation(IContentAssistant.CONTEXT_INFO_ABOVE)
         
-        assistant.enableColoredLabels(true)
+        enableColoredLabels(true)
         
-        assistant.setShowEmptyList(true)
-        
-        return assistant
+        setShowEmptyList(true)
     }
 }
 
--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/completion/KotlinCompletionUtils.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/completion/KotlinCompletionUtils.kt
@@ -16,55 +16,50 @@
  *******************************************************************************/
 package org.jetbrains.kotlin.ui.editors.completion
 
+import com.intellij.openapi.util.text.StringUtilRt
+import com.intellij.psi.PsiElement
 import org.eclipse.core.resources.IFile
 import org.eclipse.jdt.core.search.SearchPattern
-import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor
+import org.eclipse.jdt.internal.ui.JavaPlugin
+import org.eclipse.jdt.ui.PreferenceConstants
 import org.jetbrains.kotlin.core.builder.KotlinPsiManager
 import org.jetbrains.kotlin.core.log.KotlinLogger
+import org.jetbrains.kotlin.core.model.KotlinScriptEnvironment
+import org.jetbrains.kotlin.core.resolve.KotlinAnalyzer
+import org.jetbrains.kotlin.core.resolve.KotlinResolutionFacade
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptorWithVisibility
+import org.jetbrains.kotlin.descriptors.TypeParameterDescriptor
 import org.jetbrains.kotlin.eclipse.ui.utils.EditorUtil
 import org.jetbrains.kotlin.eclipse.ui.utils.LineEndUtil
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtSimpleNameExpression
-import com.intellij.openapi.util.text.StringUtilRt
-import com.intellij.psi.PsiElement
-import com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.kotlin.core.resolve.KotlinAnalyzer
-import org.eclipse.jdt.core.JavaCore
+import org.jetbrains.kotlin.resolve.scopes.DescriptorKindFilter
+import org.jetbrains.kotlin.ui.editors.KotlinEditor
 import org.jetbrains.kotlin.ui.editors.codeassist.getResolutionScope
 import org.jetbrains.kotlin.ui.editors.codeassist.isVisible
-import org.eclipse.jdt.ui.PreferenceConstants
-import org.eclipse.jdt.internal.ui.JavaPlugin
-import org.jetbrains.kotlin.descriptors.TypeParameterDescriptor
-import org.jetbrains.kotlin.descriptors.DeclarationDescriptorWithVisibility
-import org.jetbrains.kotlin.idea.codeInsight.ReferenceVariantsHelper
-import org.jetbrains.kotlin.core.model.KotlinEnvironment
-import org.jetbrains.kotlin.resolve.scopes.DescriptorKindFilter
-import org.jetbrains.kotlin.core.resolve.KotlinResolutionFacade
-import org.jetbrains.kotlin.ui.editors.KotlinEditor
-import org.jetbrains.kotlin.core.model.KotlinScriptEnvironment
 
-public object KotlinCompletionUtils {
-    private val KOTLIN_DUMMY_IDENTIFIER = "KotlinRulezzz"
+object KotlinCompletionUtils {
+    private const val KOTLIN_DUMMY_IDENTIFIER = "KotlinRulezzz"
     
-    public fun applicableNameFor(prefix: String, name: Name): Boolean {
+    fun applicableNameFor(prefix: String, name: Name): Boolean {
         return !name.isSpecial && applicableNameFor(prefix, name.identifier)
     }
     
-    public fun applicableNameFor(prefix: String, completion: String): Boolean {
+    fun applicableNameFor(prefix: String, completion: String): Boolean {
         return completion.startsWith(prefix) || 
             completion.toLowerCase().startsWith(prefix) || 
             SearchPattern.camelCaseMatch(prefix, completion)
     }
     
-    public fun getReferenceVariants(
+    fun getReferenceVariants(
             simpleNameExpression: KtSimpleNameExpression,
             nameFilter: (Name) -> Boolean,
             file: IFile,
             identifierPart: String?
     ): Collection<DeclarationDescriptor> {
-        val (analysisResult, container) = KotlinAnalyzer.analyzeFile(simpleNameExpression.getContainingKtFile())
+        val (analysisResult, container) = KotlinAnalyzer.analyzeFile(simpleNameExpression.containingKtFile)
         if (container == null) return emptyList()
         
         val inDescriptor = simpleNameExpression
@@ -73,7 +68,7 @@ public object KotlinCompletionUtils {
                 .ownerDescriptor
         
         val showNonVisibleMembers =
-            !JavaPlugin.getDefault().getPreferenceStore().getBoolean(PreferenceConstants.CODEASSIST_SHOW_VISIBLE_PROPOSALS)
+            !JavaPlugin.getDefault().preferenceStore.getBoolean(PreferenceConstants.CODEASSIST_SHOW_VISIBLE_PROPOSALS)
         
         val visibilityFilter = { descriptor: DeclarationDescriptor ->
             when (descriptor) {
@@ -90,7 +85,7 @@ public object KotlinCompletionUtils {
         val collectAll = (identifierPart == null || identifierPart.length > 2) || !KotlinScriptEnvironment.isScript(file)
         val kind = if (collectAll) DescriptorKindFilter.ALL else DescriptorKindFilter.CALLABLES
         
-        return ReferenceVariantsHelper(
+        return KotlinReferenceVariantsHelper (
                 analysisResult.bindingContext,
                 KotlinResolutionFacade(file, container, analysisResult.moduleDescriptor),
                 analysisResult.moduleDescriptor,
@@ -98,7 +93,7 @@ public object KotlinCompletionUtils {
                 simpleNameExpression, kind, nameFilter)
     }
     
-    public fun getPsiElement(editor: KotlinEditor, identOffset: Int): PsiElement? {
+    fun getPsiElement(editor: KotlinEditor, identOffset: Int): PsiElement? {
         val sourceCode = EditorUtil.getSourceCode(editor)
         val sourceCodeWithMarker = StringBuilder(sourceCode).insert(identOffset, KOTLIN_DUMMY_IDENTIFIER).toString()
         val jetFile: KtFile?
@@ -106,17 +101,13 @@ public object KotlinCompletionUtils {
         if (file != null) {
             jetFile = KotlinPsiManager.parseText(StringUtilRt.convertLineSeparators(sourceCodeWithMarker), file)
         } else {
-            KotlinLogger.logError("Failed to retrieve IFile from editor " + editor, null)
+            KotlinLogger.logError("Failed to retrieve IFile from editor $editor", null)
             return null
         }
         
         if (jetFile == null) return null
         
-        val offsetWithourCR = LineEndUtil.convertCrToDocumentOffset(sourceCodeWithMarker, identOffset, editor.document)
-        return jetFile.findElementAt(offsetWithourCR)
-    }
-    
-    public fun replaceMarkerInIdentifier(identifier: String): String {
-        return identifier.replaceFirst(KOTLIN_DUMMY_IDENTIFIER, "")
+        val offsetWithoutCR = LineEndUtil.convertCrToDocumentOffset(sourceCodeWithMarker, identOffset, editor.document)
+        return jetFile.findElementAt(offsetWithoutCR)
     }
 }
