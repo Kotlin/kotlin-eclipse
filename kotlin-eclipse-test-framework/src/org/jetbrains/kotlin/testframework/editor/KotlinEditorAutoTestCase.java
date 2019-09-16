5c75a07ebabedc0d6021b931c019a56bcaef099b
Paweł Marks <pmarks@virtuslab.com>
2019-09-16 15:25:35 +0200

    Fixes problems with tags in tests names

--- kotlin-eclipse-test-framework/src/org/jetbrains/kotlin/testframework/editor/KotlinEditorAutoTestCase.java
+++ kotlin-eclipse-test-framework/src/org/jetbrains/kotlin/testframework/editor/KotlinEditorAutoTestCase.java
@@ -16,15 +16,15 @@
  *******************************************************************************/
 package org.jetbrains.kotlin.testframework.editor;
 
+import org.jetbrains.kotlin.testframework.utils.UntaggedTestName;
 import org.junit.Rule;
-import org.junit.rules.TestName;
 
 import java.io.File;
 
 public abstract class KotlinEditorAutoTestCase extends KotlinProjectTestCase {
     
     @Rule
-    public TestName name = new TestName();
+    public UntaggedTestName name = new UntaggedTestName();
     
     private static final String TEST_DATA_PATH = "testData";
     
