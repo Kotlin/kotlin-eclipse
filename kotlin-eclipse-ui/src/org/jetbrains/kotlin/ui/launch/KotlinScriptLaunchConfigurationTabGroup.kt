e3c5dd55a8ff41f62f2270612b2ff25be81b1829
U534967 <julian.hahn@lhind.dlh.de>
2021-12-18 11:15:12 +0100

    Revert "Update Eclipse version to 2021-06"
    This reverts commit dbeb61949cbd2eac03356fe040eb95b67628c8da.

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/launch/KotlinScriptLaunchConfigurationTabGroup.kt
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/launch/KotlinScriptLaunchConfigurationTabGroup.kt
@@ -2,13 +2,10 @@ package org.jetbrains.kotlin.ui.launch
 
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup
 import org.eclipse.debug.ui.ILaunchConfigurationDialog
-import org.eclipse.debug.ui.CommonTab
-import org.eclipse.debug.ui.ILaunchConfigurationTab
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab
 
 class KotlinScriptLaunchConfigurationTabGroup : AbstractLaunchConfigurationTabGroup() {
     override fun createTabs(dialog: ILaunchConfigurationDialog, mode: String) {
-        val tabs = arrayOf(CommonTab())
-        setTabs(*tabs)
+        setTabs(arrayOf(JavaArgumentsTab()))
     }
 }
